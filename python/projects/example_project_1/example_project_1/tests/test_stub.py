43d967846fc48d46b5bb647e0a611b91d7383a8c
dbcerigo <dbcerigo@gmail.com>
2024-02-10 18:32:34 +0900

    Fix formatting for example project
    Also checks that all dev flow is working etc

--- python/projects/example_project_1/example_project_1/tests/test_stub.py
+++ python/projects/example_project_1/example_project_1/tests/test_stub.py
@@ -1,9 +1,11 @@
 """Tests."""
 from example_project_1 import stub
 
+
 def test_stub_function():
     """Test stub function."""
     stub.stub_function()
+
 
 def test_lib_stub_function():
     """Test lib stub function."""
