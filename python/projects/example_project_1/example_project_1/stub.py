542a4967636a3becf8306a6432276495b30aa7f1
dbcerigo <dbcerigo@gmail.com>
2024-02-10 17:45:15 +0900

    Add example project

---
+++ python/projects/example_project_1/example_project_1/stub.py
@@ -0,0 +1,12 @@
+"""Stub module, for demonstration purposes."""
+from example_lib_1 import stub as lib_stub
+
+
+def stub_function():
+    """Stub function, for demonstration purposes."""
+    print("Stub function")
+
+
+def lib_stub_function():
+    """Demonstrate using monorepo libraries in projects."""
+    lib_stub.stub_function()
