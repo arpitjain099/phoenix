bb861e8568d8d8c74e9c5765ce98de3088770e96
dbcerigo <dbcerigo@gmail.com>
2024-02-24 19:47:16 +0900

    FIX: missing type hints

--- python/projects/example_project_1/example_project_1/stub.py
+++ python/projects/example_project_1/example_project_1/stub.py
@@ -2,11 +2,11 @@
 from example_lib_1 import stub as lib_stub
 
 
-def stub_function():
+def stub_function() -> None:
     """Stub function, for demonstration purposes."""
     print("Stub function")
 
 
-def lib_stub_function():
+def lib_stub_function() -> None:
     """Demonstrate using monorepo libraries in projects."""
     lib_stub.stub_function()
