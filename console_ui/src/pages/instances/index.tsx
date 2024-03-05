5efb479af90cb44b5644b8a7df2250598a87e26e
Chisom Marshall Ogbuagu <36017222+chiysom@users.noreply.github.com>
2024-03-05 10:29:02 +0100

    Integration for instance list

--- console_ui/src/pages/instances/index.tsx
+++ console_ui/src/pages/instances/index.tsx
@@ -1,10 +1,118 @@
-import { MantineListInferencer } from "@refinedev/inferencer/mantine";
-import { GetServerSideProps } from "next";
+import React from "react";
+import { IResourceComponentsProps, useTranslate } from "@refinedev/core";
+import { useTable } from "@refinedev/react-table";
+import { ColumnDef, flexRender } from "@tanstack/react-table";
+import { ScrollArea, Table, Pagination, Group } from "@mantine/core";
+import {
+	List,
+	EditButton,
+	ShowButton,
+	DeleteButton,
+	DateField,
+} from "@refinedev/mantine";
 
-export default function InstancesList() {
-	return <MantineListInferencer />;
-}
+export const InstanceList: React.FC<IResourceComponentsProps> = () => {
+	const translate = useTranslate();
+	const columns = React.useMemo<ColumnDef<any>[]>(
+		() => [
+			{
+				id: "name",
+				accessorKey: "name",
+				header: translate("instances.fields.name"),
+			},
+			{
+				id: "status",
+				accessorKey: "status",
+				header: translate("instances.fields.status"),
+			},
+			{
+				id: "created_at",
+				accessorKey: "created_at",
+				header: translate("instances.fields.created_at"),
+				cell: function render({ getValue }) {
+					return <DateField value={getValue<any>()} />;
+				},
+			},
+			{
+				id: "actions",
+				accessorKey: "id",
+				header: translate("table.actions"),
+				cell: function render({ getValue }) {
+					return (
+						<Group spacing="xs" noWrap>
+							<ShowButton hideText recordItemId={getValue() as string} />
+							<EditButton hideText recordItemId={getValue() as string} />
+							<DeleteButton hideText recordItemId={getValue() as string} />
+						</Group>
+					);
+				},
+			},
+		],
+		[translate]
+	);
 
-export const getServerSideProps: GetServerSideProps<{}> = async (_context) => ({
-	props: {},
-});
+	const {
+		getHeaderGroups,
+		getRowModel,
+		setOptions,
+		refineCore: {
+			setCurrent,
+			pageCount,
+			current,
+			tableQueryResult: { data: tableData },
+		},
+	} = useTable({
+		columns,
+	});
+
+	setOptions((prev) => ({
+		...prev,
+		meta: {
+			...prev.meta,
+		},
+	}));
+
+	return (
+		<List>
+			<ScrollArea>
+				<Table highlightOnHover>
+					<thead>
+						{getHeaderGroups().map((headerGroup) => (
+							<tr key={headerGroup.id}>
+								{headerGroup.headers.map((header) => (
+									<th key={header.id}>
+										{!header.isPlaceholder &&
+											flexRender(
+												header.column.columnDef.header,
+												header.getContext()
+											)}
+									</th>
+								))}
+							</tr>
+						))}
+					</thead>
+					<tbody>
+						{getRowModel().rows.map((row) => (
+							<tr key={row.id}>
+								{row.getVisibleCells().map((cell) => (
+									<td key={cell.id}>
+										{flexRender(cell.column.columnDef.cell, cell.getContext())}
+									</td>
+								))}
+							</tr>
+						))}
+					</tbody>
+				</Table>
+			</ScrollArea>
+			<br />
+			<Pagination
+				position="right"
+				total={pageCount}
+				page={current}
+				onChange={setCurrent}
+			/>
+		</List>
+	);
+};
+
+export default InstanceList;
