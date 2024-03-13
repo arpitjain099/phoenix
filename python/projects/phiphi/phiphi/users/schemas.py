5885b22eaea3fb17da71b1ce733ae83ef9989603
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-03-13 12:55:24 +0100

    Add phiphi/users/schemas.py for create_user

---
+++ python/projects/phiphi/phiphi/users/schemas.py
@@ -0,0 +1,33 @@
+"""Schemas for the users."""
+import pydantic
+
+
+class UserBase(pydantic.BaseModel):
+    """User base schema.
+
+    Shared properties of all users.
+    """
+
+    email: pydantic.EmailStr | None = None
+    display_name: str | None = None
+
+
+class UserCreate(UserBase):
+    """User create schema.
+
+    Properties to receive via API on creation.
+    """
+
+    email: pydantic.EmailStr
+    display_name: str
+
+
+class User(UserBase):
+    """User schema.
+
+    Properties to return to client.
+    """
+
+    model_config = pydantic.ConfigDict(from_attributes=True)
+
+    id: int
