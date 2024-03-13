fadb63f570bccf701cb811db1fd754c74b87698e
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-03-13 12:51:50 +0100

    Add phiphi/tests/users/test_users::test_create_user
    The implementation of this will be done in the following commits.
    
    TDD baby :).

--- python/projects/phiphi/phiphi/tests/users/test_users.py
+++ python/projects/phiphi/phiphi/tests/users/test_users.py
@@ -1,5 +1,6 @@
 """Test Users."""
 import sqlalchemy
+from fastapi.testclient import TestClient
 
 from phiphi.users import models
 
@@ -10,3 +11,13 @@ def test_user(session: sqlalchemy.orm.Session) -> None:
     count = response.one()
     assert count
     assert count[0] == 0
+
+
+def test_create_user(recreate_tables, client: TestClient) -> None:
+    """Test creating a user."""
+    data = {"email": "test@test.com", "display_name": "test"}
+    response = client.post("/users/", json=data)
+    assert response.status_code == 200
+    user = response.json()
+    assert user["email"] == data["email"]
+    assert user["display_name"] == data["display_name"]
