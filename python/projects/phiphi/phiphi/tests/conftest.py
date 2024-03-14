330369bf61afc59909b99b2c99d8ffa7e87984d7
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-03-14 14:42:08 +0100

    Refactor conftest.client to have a test_app fixture
    Refactor so that we can create clients that have different
    authentication.

--- python/projects/phiphi/phiphi/tests/conftest.py
+++ python/projects/phiphi/phiphi/tests/conftest.py
@@ -12,8 +12,8 @@ from phiphi.seed import main as seed_main
 
 
 @pytest.fixture(scope="session")
-def client(session) -> Generator[TestClient, None, None]:
-    """Client for testing.
+def test_app(session) -> Generator[main.FastAPI, None, None]:
+    """Initialise the test app.
 
     Includes overriding the get_session dependency to use the session fixture. This is necessary to
     use the test sessions in the app.
@@ -23,7 +23,13 @@ def client(session) -> Generator[TestClient, None, None]:
         yield session
 
     main.app.dependency_overrides[db.get_session] = override_get_session
-    with TestClient(main.app) as client:
+    yield main.app
+
+
+@pytest.fixture(scope="session")
+def client(test_app) -> Generator[TestClient, None, None]:
+    """Client for testing."""
+    with TestClient(test_app) as client:
         yield client
 
 
