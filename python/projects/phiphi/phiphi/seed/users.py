78ab059debfd0fb6517408684f94ef7b23203fe6
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-03-15 13:48:05 +0100

    Refactor seed.users so that it has TEST_USER_1_CREATE
    This variable will be used in tests.

--- python/projects/phiphi/phiphi/seed/users.py
+++ python/projects/phiphi/phiphi/seed/users.py
@@ -3,11 +3,16 @@ from sqlalchemy.orm import Session
 
 from phiphi.users import crud, schemas
 
+# Quick way of allowing tests to check this user.
+# If more objects are added like this it Might be a good idea to refactor this so that all seeded
+# objects are added to a registry that seed.main returns.
+TEST_USER_1_CREATE = schemas.UserCreate(email="test1@phiphi.com", display_name="Test User 1")
+
 
 def seed_test_users(session: Session) -> None:
     """Seed the users."""
     users = [
-        schemas.UserCreate(email="test1@phiphi.com", display_name="Test User 1"),
+        TEST_USER_1_CREATE,
         schemas.UserCreate(email="test2@phiphi.com", display_name="Test User 2"),
     ]
     for user in users:
