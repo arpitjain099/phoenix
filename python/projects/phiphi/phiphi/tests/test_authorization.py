2f7a2efd6ad6c6c583c189ef0b94a9343a1a1e11
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-03-15 13:52:54 +0100

    Add USE_COOKIE_AUTH to False for authorization tests

--- python/projects/phiphi/phiphi/tests/test_authorization.py
+++ python/projects/phiphi/phiphi/tests/test_authorization.py
@@ -1,17 +1,24 @@
 """Authorization test."""
+import pytest
 from fastapi.testclient import TestClient
 
 from phiphi.core import config
 
 
-def test_read_me_no_header(client: TestClient, reseed_tables) -> None:
+# Making sure that the settings are not using cookie auth so is consistent with a production env
+@pytest.mark.patch_settings({"USE_COOKIE_AUTH": False})
+def test_read_me_no_header(client: TestClient, reseed_tables, patch_settings) -> None:
     """Test authorization users/me with no header."""
     response = client.get("/users/me")
     assert response.status_code == 401
     assert response.json() == {"detail": "Cannot authenticate."}
 
 
-def test_read_me_first_admin(client_first_admin_user: TestClient, reseed_tables) -> None:
+# Making sure that the settings are not using cookie auth so is consistent with a production env
+@pytest.mark.patch_settings({"USE_COOKIE_AUTH": False})
+def test_read_me_first_admin(
+    client_first_admin_user: TestClient, reseed_tables, patch_settings
+) -> None:
     """Test authorization users/me with header for admin."""
     response = client_first_admin_user.get("/users/me")
     assert response.status_code == 200
