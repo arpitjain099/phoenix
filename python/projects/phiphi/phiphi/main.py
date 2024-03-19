481313a94347de781122f0a803640c1e9a6c984f
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-03-19 13:37:33 +0100

    Rename phiphi/health.py to phiphi/health_check.py

--- python/projects/phiphi/phiphi/main.py
+++ python/projects/phiphi/phiphi/main.py
@@ -1,9 +1,9 @@
 """Main for phiphi."""
 from fastapi import FastAPI
 
-from phiphi import health
+from phiphi import health_check
 from phiphi.core import config
 
 app = FastAPI(title=config.settings.TITLE)
 
-app.include_router(health.router)
+app.include_router(health_check.router)
