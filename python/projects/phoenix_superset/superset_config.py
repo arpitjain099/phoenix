e9592671165ff3d0835f341af18aad747fda228a
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-03-29 08:47:06 +0100

    Add of superset_config.py

---
+++ python/projects/phoenix_superset/superset_config.py
@@ -0,0 +1,15 @@
+"""Phoenix superset config."""
+import os
+
+from flask_appbuilder.security.manager import AUTH_REMOTE_USER  # type: ignore[import-untyped]
+from phoenix_superset import custom_sso_manager
+
+AUTH_TYPE = AUTH_REMOTE_USER
+
+# Default of x-auth-request-email is what oauth2-proxy set up uses
+AUTH_REMOTE_USER_ENV_VAR = os.getenv("AUTH_REMOTE_USER_ENV_VAR", "x-auth-request-email")
+
+# This is needed to test the system without the authentication layer
+LOGIN_REDIRECT_URL = os.getenv("LOGIN_REDIRECT_URL")
+
+CUSTOM_SECURITY_MANAGER = custom_sso_manager.PhoenixCustomSsoSecurityManager
