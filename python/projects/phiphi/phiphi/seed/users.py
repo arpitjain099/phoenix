43fdeb6bcb1a053bf30cb97370c2d7d3893654f9
Mba Gozpel <gosper4u@gmail.com>
2024-03-29 13:30:17 +0100

    Refactor user.app_role into an enum

--- python/projects/phiphi/phiphi/seed/users.py
+++ python/projects/phiphi/phiphi/seed/users.py
@@ -7,16 +7,14 @@ from phiphi.api.users import crud, schemas
 # Quick way of allowing tests to check this user.
 # If more objects are added like this it Might be a good idea to refactor this so that all seeded
 # objects are added to a registry that seed.main returns.
-TEST_USER_1_CREATE = schemas.UserCreate(
-    email="test1@phiphi.com", display_name="Test User 1", app_role="admin"
-)
+TEST_USER_1_CREATE = schemas.UserCreate(email="test1@phiphi.com", display_name="Test User 1")
 
 
 def seed_test_users(session: Session) -> None:
     """Seed the users."""
     users = [
         TEST_USER_1_CREATE,
-        schemas.UserCreate(email="test2@phiphi.com", display_name="Test User 2", app_role="admin"),
+        schemas.UserCreate(email="test2@phiphi.com", display_name="Test User 2"),
     ]
     for user in users:
         crud.create_user(session=session, user=user)
@@ -29,6 +27,7 @@ def init_first_admin_user(session: Session) -> schemas.User:
         user_in = schemas.UserCreate(
             email=config.settings.FIRST_ADMIN_USER_EMAIL,
             display_name=config.settings.FIRST_ADMIN_USER_DISPLAY_NAME,
+            app_role=schemas.AppRole.admin,
         )
         user = crud.create_user(session=session, user=user_in)
     return user
