66f02cbf134b9f1792c2edb91b7a642a98389271
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-03-29 11:40:16 +0100

    Remove schemas from phiphi
    Includes removing the migrations with the schemas
    
    This has been done because we starting having problems with alembic and
    it "seeing" the tables in the schema `platform`. My feeling is that this
    change is adding quiet a bit of difficult functionality that we are
    currently not using. If we use schemas in the future we can look at this
    functionality then.

--- python/projects/phiphi/phiphi/platform_db.py
+++ python/projects/phiphi/phiphi/platform_db.py
@@ -2,27 +2,16 @@
 import logging
 from typing import Generator
 
-from sqlalchemy import DDL, MetaData, create_engine, event
+from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, Session
 
 from phiphi import config
 
 logger = logging.getLogger(__name__)
 
-SCHEMA = "platform"
-
 
 class Base(DeclarativeBase):
-    """Base model.
-
-    This will create all models in the schema `platform`.
-    """
-
-    metadata = MetaData(schema=SCHEMA)
-
-
-# This is needed so that create_all will also create the schema
-event.listen(Base.metadata, "before_create", DDL("CREATE SCHEMA IF NOT EXISTS " + SCHEMA))
+    """Base model."""
 
 
 engine = create_engine(str(config.settings.SQLALCHEMY_DATABASE_URI))
