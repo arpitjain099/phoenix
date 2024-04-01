bbbe8447050079039db3c76189950977b5a107d2
Mba Gozpel <gosper4u@gmail.com>
2024-04-01 09:26:35 +0100

    add migration revision for instance

--- python/projects/phiphi/phiphi/all_platform_models.py
+++ python/projects/phiphi/phiphi/all_platform_models.py
@@ -4,7 +4,7 @@ This module is used to import all models of the application to be used in the
 Alembic migrations and testing.
 """
 from phiphi import platform_db
-from phiphi.api.users import models  # noqa: F401
+from phiphi.api.instances import models  # noqa: F401
 
 Base = platform_db.Base
 base_metadata = Base.metadata
