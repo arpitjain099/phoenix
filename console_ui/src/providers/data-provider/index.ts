91cf4c26fe4bb633a6e903eaaaa6e36d7b66b959
Chisom Marshall Ogbuagu <36017222+chiysom@users.noreply.github.com>
2024-04-02 08:45:35 +0100

    Setup environment variables

--- console_ui/src/providers/data-provider/index.ts
+++ console_ui/src/providers/data-provider/index.ts
@@ -1,6 +1,6 @@
 import dataProviderSimpleRest from "@refinedev/simple-rest";
 
-const API_URL = "http://localhost:8080";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
 
 const dataProvider = dataProviderSimpleRest(API_URL);
 
