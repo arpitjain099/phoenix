b1afd00c9a76c12530bee9fff9269a515d208efa
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-04-04 13:08:47 +0200

    Refactor config.py in phoenix_superset/Dockerfile
    Two changes have been made here:
    1. The removal of SUPERSET_CONFIG_PATH from the Dockerfile so. This is
       needed so that when the image is run in the helm configuration
       overrides can be configured in the helm chart.
    2. Improve the naming for the configuration so it is clear that is part
       of phoenix_superset and not default superset.

--- python/projects/phoenix_superset/config.py
+++ python/projects/phoenix_superset/config.py
@@ -7,9 +7,10 @@ from phoenix_superset import custom_sso_manager
 AUTH_TYPE = AUTH_REMOTE_USER
 
 # Default of x-auth-request-email is what oauth2-proxy set up uses
-AUTH_REMOTE_USER_ENV_VAR = os.getenv("AUTH_REMOTE_USER_ENV_VAR", "x-auth-request-email")
+AUTH_REMOTE_USER_ENV_VAR = os.getenv("AUTH_REMOTE_USER_ENV_VAR", "HTTP_X_AUTH_REQUEST_EMAIL")
 
 # This is needed to test the system without the authentication layer
 LOGIN_REDIRECT_URL = os.getenv("LOGIN_REDIRECT_URL")
+AUTH_USER_REGISTRATION = True
 
 CUSTOM_SECURITY_MANAGER = custom_sso_manager.PhoenixCustomSSOSecurityManager
