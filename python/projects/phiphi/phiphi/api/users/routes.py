7ca3981ee444df1c2af00cf19e89b5d1b12c0060
Mba Gozpel <gosper4u@gmail.com>
2024-04-08 08:32:47 +0100

    change schemas.User to schemas.UserResponse

--- python/projects/phiphi/phiphi/api/users/routes.py
+++ python/projects/phiphi/phiphi/api/users/routes.py
@@ -9,20 +9,20 @@ router = fastapi.APIRouter()
 
 # It is important that this route is defined before the /users/{user_id} route or me will be
 # interpreted as a user id
-@router.get("/users/me", response_model=schemas.User)
-def read_me(current_user: deps.CurrentUser) -> schemas.User:
+@router.get("/users/me", response_model=schemas.UserResponse)
+def read_me(current_user: deps.CurrentUser) -> schemas.UserResponse:
     """Get the current users."""
     return current_user
 
 
-@router.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, session: deps.SessionDep) -> schemas.User:
+@router.post("/users/", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, session: deps.SessionDep) -> schemas.UserResponse:
     """Create a new user."""
     return crud.create_user(session, user)
 
 
-@router.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, session: deps.SessionDep) -> schemas.User:
+@router.get("/users/{user_id}", response_model=schemas.UserResponse)
+def read_user(user_id: int, session: deps.SessionDep) -> schemas.UserResponse:
     """Read a user."""
     user = crud.read_user(session, user_id)
     if user is None:
@@ -30,14 +30,18 @@ def read_user(user_id: int, session: deps.SessionDep) -> schemas.User:
     return user
 
 
-@router.get("/users/", response_model=list[schemas.User])
-def read_users(session: deps.SessionDep, start: int = 0, end: int = 100) -> list[schemas.User]:
+@router.get("/users/", response_model=list[schemas.UserResponse])
+def read_users(
+    session: deps.SessionDep, start: int = 0, end: int = 100
+) -> list[schemas.UserResponse]:
     """Retrieve users."""
     return crud.read_users(session, start, end)
 
 
-@router.put("/users/{user_id}", response_model=schemas.User)
-def update_user(user_id: int, user: schemas.UserUpdate, session: deps.SessionDep) -> schemas.User:
+@router.put("/users/{user_id}", response_model=schemas.UserResponse)
+def update_user(
+    user_id: int, user: schemas.UserUpdate, session: deps.SessionDep
+) -> schemas.UserResponse:
     """Update a user."""
     updated_user = crud.update_user(session, user_id, user)
     if updated_user is None:
