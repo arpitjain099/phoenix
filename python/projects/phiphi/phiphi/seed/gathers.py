9b0f606e5b5cc84c61f00736895030a2aca52c82
Mba Gozpel <gosper4u@gmail.com>
2024-04-08 11:29:24 +0100

    add test and seeds for gather

---
+++ python/projects/phiphi/phiphi/seed/gathers.py
@@ -0,0 +1,40 @@
+"""Seed the gathers."""
+import datetime
+
+from sqlalchemy.orm import Session
+
+from phiphi.api.gathers import crud, schemas
+
+TEST_APIFY_GATHER_CREATE = schemas.ApifyGatherCreate(
+    description="Phoenix Apify Gather",
+    config_type=schemas.GatherType.apify_facebook_messages,
+    instance_id=1,
+    input_type=schemas.ApifyGatherInputType.author_url_list,
+    input_data="lorem ipsum",
+    start_date=datetime.datetime.now(),
+    end_date=datetime.datetime.now(),
+    limit_messages=1000,
+    limit_replies=100,
+    nested_replies=False,
+)
+
+TEST_APIFY_GATHER_CREATE_2 = schemas.ApifyGatherCreate(
+    description="Phoenix Apify Gather 2",
+    config_type=schemas.GatherType.apify_facebook_messages,
+    instance_id=1,
+    input_type=schemas.ApifyGatherInputType.author_url_list,
+    input_data="lorem ipsum",
+    start_date=datetime.datetime.now(),
+    end_date=datetime.datetime.now(),
+    limit_messages=1000,
+    limit_replies=100,
+    nested_replies=False,
+)
+
+
+def seed_test_apify_gathers(session: Session) -> None:
+    """Seed the gathers."""
+    apify_gathers = [TEST_APIFY_GATHER_CREATE, TEST_APIFY_GATHER_CREATE_2]
+
+    for apify_gather in apify_gathers:
+        crud.create_apify_gather(session=session, gather_data=apify_gather)
--- python/projects/phiphi/phiphi/tests/api/gathers/test_gathers.py
+++ python/projects/phiphi/phiphi/tests/api/gathers/test_gathers.py
@@ -9,66 +9,80 @@ CREATED_TIME = "2024-04-01T12:00:01"
 UPDATE_TIME = "2024-04-01T12:00:02"
 
 
-def test_get_gathers(client: TestClient, reseed_tables) -> None:
+def test_get_apify_gathers(client: TestClient, reseed_tables) -> None:
     """Test getting gathers."""
-    response = client.get("/gathers/")
+    response = client.get("/gathers/apify")
     assert response.status_code == 200
     gathers = response.json()
     assert len(gathers) == 2
 
 
 @pytest.mark.freeze_time(UPDATE_TIME)
-def test_update_gather(client: TestClient, reseed_tables, session: sqlalchemy.orm.Session) -> None:
+def test_update_apify_gather(
+    client: TestClient, reseed_tables, session: sqlalchemy.orm.Session
+) -> None:
     """Test updating a gather."""
-    data = {"description": "New gather"}
+    data = {"mark_to_delete": True}
     gather_id = 1
-    response = client.put(f"/gathers/{gather_id}", json=data)
+    response = client.put(f"/gathers/apify/{gather_id}", json=data)
     assert response.status_code == 200
     gather = response.json()
-    assert gather["description"] == data["description"]
+    assert gather["mark_to_delete"] == data["mark_to_delete"]
     db_gather = session.get(models.Gather, gather_id)
     assert db_gather
-    assert db_gather.description == data["description"]
+    assert db_gather.mark_to_delete == data["mark_to_delete"]
     assert db_gather.updated_at.isoformat() == UPDATE_TIME
 
 
 def test_update_gather_not_found(client: TestClient, recreate_tables) -> None:
     """Test updating a gather that does not exist."""
-    data = {"description": "New gather"}
-    response = client.put("/gathers/100", json=data)
+    data = {"mark_to_delete": True}
+    response = client.put("/gathers/apify/100", json=data)
     assert response.status_code == 404
-    assert response.json() == {"detail": "Instance not found"}
+    assert response.json() == {"detail": "Gather not found"}
 
 
 @pytest.mark.freeze_time(CREATED_TIME)
-def test_create_get_gather(recreate_tables, client: TestClient) -> None:
+def test_create_get_apify_gather(recreate_tables, client: TestClient) -> None:
     """Test create and then get of a gather."""
     data = {
-        "description": "Phoenix gather",
+        "description": "Firt apify gather",
         "config_type": "apify_facebook_messages",
-        "config": {
-            "start_date": CREATED_TIME,
-            "end_date": UPDATE_TIME,
-            "limit_messages": 1000,
-            "limit_replies": 100,
-            "nested_replies": False,
-            "config_input": {"input_type": "author_url_list", "data": ""},
-        },
+        "instance_id": 1,
+        "input_type": "author_url_list",
+        "input_data": "lorem ipsum",
+        "start_date": "2024-04-08T08:41:05",
+        "end_date": "2024-04-08T08:41:05",
+        "limit_messages": 1000,
+        "limit_replies": 100,
+        "nested_replies": False,
     }
-    response = client.post("/gathers/", json=data)
+    response = client.post("/gathers/apify", json=data)
     assert response.status_code == 200
     gather = response.json()
     assert gather["description"] == data["description"]
     assert gather["config_type"] == data["config_type"]
-    assert gather["config"] == data["config"]
+    assert gather["input_type"] == data["input_type"]
+    assert gather["instance_id"] == data["instance_id"]
+    assert gather["input_data"] == data["input_data"]
+    assert gather["start_date"] == data["start_date"]
+    assert gather["end_date"] == data["end_date"]
+    assert gather["limit_messages"] == data["limit_messages"]
+    assert gather["limit_replies"] == data["limit_replies"]
     assert gather["created_at"] == CREATED_TIME
 
-    response = client.get(f"/gathers/{gather['id']}")
+    response = client.get(f"/gathers/apify/{gather['id']}")
     assert response.status_code == 200
 
     gather = response.json()
 
     assert gather["description"] == data["description"]
-    assert gather["created_at"] == CREATED_TIME
     assert gather["config_type"] == data["config_type"]
-    assert gather["config"] == data["config"]
+    assert gather["input_type"] == data["input_type"]
+    assert gather["instance_id"] == data["instance_id"]
+    assert gather["input_data"] == data["input_data"]
+    assert gather["start_date"] == data["start_date"]
+    assert gather["end_date"] == data["end_date"]
+    assert gather["limit_messages"] == data["limit_messages"]
+    assert gather["limit_replies"] == data["limit_replies"]
+    assert gather["created_at"] == CREATED_TIME
