1909b96612e622fdd1135f00fd685167e537e677
Mba Gozpel <gosper4u@gmail.com>
2024-04-10 11:49:18 +0100

    update seed and tests

--- python/projects/phiphi/phiphi/seed/environments.py
+++ python/projects/phiphi/phiphi/seed/environments.py
@@ -3,9 +3,13 @@ from sqlalchemy.orm import Session
 
 from phiphi.api.environments import crud, schemas
 
-TEST_ENVIRONMENT_CREATE = schemas.EnvironmentCreate(name="Phoenix", description="Environment 1")
+TEST_ENVIRONMENT_CREATE = schemas.EnvironmentCreate(
+    name="Phoenix", description="Environment 1", slug="phoenix"
+)
 
-TEST_ENVIRONMENT_CREATE_2 = schemas.EnvironmentCreate(name="Phoenix", description="Environment 2")
+TEST_ENVIRONMENT_CREATE_2 = schemas.EnvironmentCreate(
+    name="Phoenix", description="Environment 2", slug="phoenix-1234"
+)
 
 
 def seed_test_environment(session: Session) -> None:
--- python/projects/phiphi/phiphi/tests/api/environments/test_environments.py
+++ python/projects/phiphi/phiphi/tests/api/environments/test_environments.py
@@ -3,22 +3,7 @@ import pytest
 import sqlalchemy
 from fastapi.testclient import TestClient
 
-from phiphi.api.environments import crud, models
-
-
-def test_get_environment_by_unique_id(session: sqlalchemy.orm.Session, reseed_tables) -> None:
-    """Test getting an environment by unique id."""
-    environment = crud.get_environment_by_unique_id(session, "Phoenix")
-    assert environment
-
-
-def test_delete_environment(
-    session: sqlalchemy.orm.Session, reseed_tables, client: TestClient
-) -> None:
-    """Test deleting an environment."""
-    response = client.delete("/environments/1")
-    assert response.status_code == 200
-
+from phiphi.api.environments import models
 
 CREATED_TIME = "2024-04-01T12:00:01"
 UPDATE_TIME = "2024-04-01T12:00:02"
@@ -27,27 +12,29 @@ UPDATE_TIME = "2024-04-01T12:00:02"
 @pytest.mark.freeze_time(CREATED_TIME)
 def test_create_get_environment(recreate_tables, client: TestClient) -> None:
     """Test creating and get of an environment."""
-    data = {"description": "My environment", "name": "test_env"}
+    data = {"description": "My environment", "name": "test", "slug": "test-env"}
     response = client.post("/environments/", json=data)
     assert response.status_code == 200
     environment = response.json()
     assert environment["description"] == data["description"]
     assert environment["name"] == data["name"]
+    assert environment["slug"] == data["slug"]
     assert environment["created_at"] == CREATED_TIME
 
-    response = client.get(f"/environments/{environment['id']}")
+    response = client.get(f"/environments/{environment['slug']}")
     assert response.status_code == 200
 
     environment = response.json()
 
     assert environment["description"] == data["description"]
     assert environment["name"] == data["name"]
+    assert environment["slug"] == data["slug"]
     assert environment["created_at"] == CREATED_TIME
 
 
 def test_get_environment_not_found(client: TestClient, recreate_tables) -> None:
     """Test getting an environment that does not exist."""
-    response = client.get("/environments/1")
+    response = client.get("/environments/boys")
     assert response.status_code == 404
     assert response.json() == {"detail": "Environment not found"}
 
@@ -85,19 +72,16 @@ def test_update_environment_not_found(client: TestClient, recreate_tables) -> No
     assert response.json() == {"detail": "Environment not found"}
 
 
-def test_unique_id_different_with_same_name(recreate_tables, client: TestClient) -> None:
+def test_slug_already_exists(recreate_tables, client: TestClient) -> None:
     """Test that the unique Id will be different for environments with the same name."""
-    data = {"description": "test env", "name": "Test"}
+    data = {"description": "test env", "name": "test", "slug": "test"}
     response = client.post("/environments/", json=data)
     assert response.status_code == 200
     environment_1 = response.json()
     assert environment_1["description"] == data["description"]
     assert environment_1["name"] == data["name"]
+    assert environment_1["slug"] == data["slug"]
 
-    data_2 = {"description": "test env 2", "name": "Test"}
+    data_2 = {"description": "test env 2", "name": "test", "slug": "test"}
     response = client.post("/environments/", json=data_2)
-    assert response.status_code == 200
-
-    environment_2 = response.json()
-
-    assert environment_1["unique_id"] != environment_2["unique_id"]
+    assert response.status_code == 400
