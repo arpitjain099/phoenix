b2750bdacc0b8a083eb406d4bbf534182e6e243a
Chisom Marshall Ogbuagu <36017222+chiysom@users.noreply.github.com>
2024-04-11 06:53:31 +0100

    JSDoc comment for FilterDate

--- console_ui/src/utils/index.ts
+++ console_ui/src/utils/index.ts
@@ -1,4 +1,9 @@
 /* eslint-disable import/prefer-default-export */
+/**
+ * Formats the given date string into a human-readable date format.
+ * @param {string} activeDate - The date string to be formatted.
+ * @returns {string} A human-readable representation of the date.
+ */
 export const filterDate = (activeDate: string) => {
 	const fd = new Date(activeDate);
 	return fd.toDateString();
