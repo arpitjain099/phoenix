14711db9bd781e182ca058d7666fbe0a603d0450
Chisom Marshall Ogbuagu <36017222+chiysom@users.noreply.github.com>
2024-04-12 16:04:20 +0100

    prettier clean up

--- console_ui/src/interfaces/interface.ts
+++ console_ui/src/interfaces/interface.ts
@@ -1,8 +1,8 @@
 export type InstanceSchema = {
-  name: string;
-  description: string;
-  environment_id: string;
-  days_until_pi_expiration: number;
-  days_until_all_data_expiration: number;
-  expected_usage: string;
-}
+	name: string;
+	description: string;
+	environment_id: string;
+	days_until_pi_expiration: number;
+	days_until_all_data_expiration: number;
+	expected_usage: string;
+};
