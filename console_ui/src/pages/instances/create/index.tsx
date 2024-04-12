e48046382e9f2d048781329659fb461d40bc49e1
Chisom Marshall Ogbuagu <36017222+chiysom@users.noreply.github.com>
2024-04-12 16:01:00 +0100

    Switch to form component

--- console_ui/src/pages/instances/create/index.tsx
+++ console_ui/src/pages/instances/create/index.tsx
@@ -1,18 +1,9 @@
-import { IResourceComponentsProps, useTranslate } from "@refinedev/core";
+import CreateEditInstanceForm from "@components/forms/create_edit_instance";
+import { IResourceComponentsProps } from "@refinedev/core";
 import { Create, useForm } from "@refinedev/mantine";
-import {
-	TextInput,
-	NumberInput,
-	Select,
-	Textarea,
-	Alert,
-	Tooltip,
-} from "@mantine/core";
 import React from "react";
-import { IconInfoCircle } from "@tabler/icons";
 
 export const InstanceCreate: React.FC<IResourceComponentsProps> = () => {
-	const translate = useTranslate();
 	const {
 		getInputProps,
 		saveButtonProps,
@@ -47,94 +38,7 @@ export const InstanceCreate: React.FC<IResourceComponentsProps> = () => {
 
 	return (
 		<Create isLoading={formLoading} saveButtonProps={saveButtonProps}>
-			<TextInput
-				mt="sm"
-				withAsterisk
-				label={translate("instances.fields.name")}
-				{...getInputProps("name")}
-			/>
-			<Select
-				mt="sm"
-				withAsterisk
-				label={translate("instances.fields.environment_id")}
-				{...getInputProps("environment_id")}
-				data={[
-					{ label: translate("inputs.select"), value: "" },
-					{ label: "Main", value: "main" },
-				]}
-			/>
-			<NumberInput
-				mt="sm"
-				min={183}
-				max={365}
-				label={
-					<div className="flex items-center">
-						<Tooltip
-							label={translate("instances.warnings.days_until_pi_expiration")}
-						>
-							<span className="flex">
-								<IconInfoCircle size={12} />
-							</span>
-						</Tooltip>
-						{translate("instances.fields.days_until_pi_expiration")}
-						<span className="text-red-500">*</span>
-					</div>
-				}
-				{...getInputProps("days_until_pi_expiration")}
-			/>
-			<NumberInput
-				mt="sm"
-				min={183}
-				max={365}
-				label={
-					<div className="flex items-center">
-						<Tooltip
-							label={translate(
-								"instances.warnings.days_until_all_data_expiration"
-							)}
-						>
-							<span className="flex">
-								<IconInfoCircle size={12} />
-							</span>
-						</Tooltip>
-						{translate("instances.fields.days_until_all_data_expiration")}
-						<span className="text-red-500">*</span>
-					</div>
-				}
-				{...getInputProps("days_until_all_data_expiration")}
-			/>
-			<Select
-				mt="sm"
-				label={translate("instances.fields.expected_usage.title")}
-				data={[
-					{ label: translate("inputs.select"), value: "" },
-					{
-						label: translate("instances.fields.expected_usage.options.one_off"),
-						value: "one_off",
-					},
-					{
-						label: translate("instances.fields.expected_usage.options.monthly"),
-						value: "monthly",
-					},
-					{
-						label: translate("instances.fields.expected_usage.options.weekly"),
-						value: "weekly",
-					},
-					{
-						label: translate("instances.fields.expected_usage.options.daily"),
-						value: "daily",
-					},
-				]}
-				{...getInputProps("expected_usage")}
-			/>
-			<Textarea
-				mt="sm"
-				label={translate("instances.fields.description")}
-				{...getInputProps("description")}
-			/>
-			<Alert mt="lg" title={translate("note")} color="gray">
-				{translate("instances.warnings.create")}
-			</Alert>
+			<CreateEditInstanceForm getInputProps={getInputProps} />
 		</Create>
 	);
 };
--- console_ui/src/pages/instances/edit/[id].tsx
+++ console_ui/src/pages/instances/edit/[id].tsx
@@ -1,22 +1,9 @@
 import React from "react";
-import {
-	IResourceComponentsProps,
-	useTranslate,
-	useUpdate,
-} from "@refinedev/core";
+import { IResourceComponentsProps, useUpdate } from "@refinedev/core";
 import { Edit, useForm } from "@refinedev/mantine";
-import {
-	TextInput,
-	NumberInput,
-	Select,
-	Textarea,
-	Alert,
-	Tooltip,
-} from "@mantine/core";
-import { IconInfoCircle } from "@tabler/icons";
+import CreateEditInstanceForm from "@components/forms/create_edit_instance";
 
 export const InstanceEdit: React.FC<IResourceComponentsProps> = () => {
-	const translate = useTranslate();
 	const { mutate } = useUpdate();
 	const {
 		getInputProps,
@@ -53,82 +40,7 @@ export const InstanceEdit: React.FC<IResourceComponentsProps> = () => {
 			saveButtonProps={{ ...saveButtonProps, onClick: handleSave }}
 			canDelete={false}
 		>
-			<TextInput
-				mt="sm"
-				withAsterisk
-				label={translate("instances.fields.name")}
-				{...getInputProps("name")}
-			/>
-			<Select
-				mt="sm"
-				withAsterisk
-				label={translate("instances.fields.environment_id")}
-				data={[
-					{ label: translate("inputs.select"), value: "" },
-					{ label: "Main", value: "main" },
-				]}
-				{...getInputProps("environment_id")}
-			/>
-			<NumberInput
-				mt="sm"
-				min={183}
-				max={365}
-				label={
-					<div className="flex items-center">
-						<Tooltip
-							label={translate("instances.warnings.days_until_pi_expiration")}
-						>
-							<span className="flex">
-								<IconInfoCircle size={12} />
-							</span>
-						</Tooltip>
-						{translate("instances.fields.days_until_pi_expiration")}
-						<span className="text-red-500">*</span>
-					</div>
-				}
-				{...getInputProps("pi_deleted_after_days")}
-			/>
-			<NumberInput
-				mt="sm"
-				min={183}
-				max={365}
-				label={
-					<div className="flex items-center">
-						<Tooltip
-							label={translate(
-								"instances.warnings.days_until_all_data_expiration"
-							)}
-						>
-							<span className="flex">
-								<IconInfoCircle size={12} />
-							</span>
-						</Tooltip>
-						{translate("instances.fields.days_until_all_data_expiration")}
-						<span className="text-red-500">*</span>
-					</div>
-				}
-				{...getInputProps("delete_after_days")}
-			/>
-			<Select
-				mt="sm"
-				label={translate("instances.fields.expected_usage")}
-				data={[
-					{ label: translate("inputs.select"), value: "" },
-					{ label: "One off analysis", value: "one_off" },
-					{ label: "Monthly", value: "monthly" },
-					{ label: "Weekly", value: "weekly" },
-					{ label: "Daily", value: "daily" },
-				]}
-				{...getInputProps("expected_usage")}
-			/>
-			<Textarea
-				mt="sm"
-				label={translate("instances.fields.description")}
-				{...getInputProps("description")}
-			/>
-			<Alert mt="lg" title={translate("note")} color="gray">
-				{translate("instances.warnings.create")}
-			</Alert>
+			<CreateEditInstanceForm getInputProps={getInputProps} />
 		</Edit>
 	);
 };
