4792a51e897ecd7a24e896b316e770a716a1c2bb
Chisom Marshall Ogbuagu <36017222+chiysom@users.noreply.github.com>
2024-04-16 09:08:05 +0100

    Add Link to Gather

--- console_ui/src/pages/instances/show/[id].tsx
+++ console_ui/src/pages/instances/show/[id].tsx
@@ -14,6 +14,7 @@ import {
 } from "@refinedev/mantine";
 import { Button, Group, Title } from "@mantine/core";
 import { IconEye } from "@tabler/icons";
+import Link from "next/link";
 
 export const InstanceShow: React.FC<IResourceComponentsProps> = () => {
 	const translate = useTranslate();
@@ -72,7 +73,7 @@ export const InstanceShow: React.FC<IResourceComponentsProps> = () => {
 			</Group>
 			<Group>
 				<Title my="xs" order={5}>
-					{translate("instances.fields.expected_usage")}:
+					{translate("instances.fields.expected_usage.title")}:
 				</Title>
 				<TextField value={record?.expected_usage} />
 			</Group>
@@ -89,6 +90,14 @@ export const InstanceShow: React.FC<IResourceComponentsProps> = () => {
 				<DateField value={record?.updated_at} />
 			</Group>
 			<Group className="mt-4 flex flex-col items-start gap-4">
+				<Link
+					href={{
+						pathname: `/instances/[instanceid]/gathers`,
+						query: { instanceid: idFromParams },
+					}}
+				>
+					<Button>Gathers</Button>
+				</Link>
 				{record?.status !== "pending" && (
 					<Button leftIcon={<IconEye />}>Go to Visualisation</Button>
 				)}
