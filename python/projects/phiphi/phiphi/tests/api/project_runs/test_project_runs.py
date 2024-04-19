d9508ebf01d8b606a7363dca84904ae65d16566e
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-04-19 10:26:36 +0200

    Rename instance to projects phiphi test

--- python/projects/phiphi/phiphi/tests/api/project_runs/test_project_runs.py
+++ python/projects/phiphi/phiphi/tests/api/project_runs/test_project_runs.py
@@ -1,46 +1,46 @@
-"""Test Instance Runs."""
+"""Test Project Runs."""
 import datetime
 
 import pytest
 import sqlalchemy
 from fastapi.testclient import TestClient
 
-from phiphi.api.instances.instance_runs import crud, schemas
+from phiphi.api.projects.project_runs import crud, schemas
 
 CREATED_TIME = "2024-04-01T12:00:01"
 UPDATE_TIME = "2024-04-01T12:00:02"
 
 
 @pytest.mark.freeze_time(CREATED_TIME)
-def test_create_get_instance_runs(reseed_tables, client: TestClient) -> None:
-    """Test create and then get of an instance run."""
-    response = client.post("/instances/1/runs/")
+def test_create_get_project_runs(reseed_tables, client: TestClient) -> None:
+    """Test create and then get of an project run."""
+    response = client.post("/projects/1/runs/")
     assert response.status_code == 200
-    instance_runs = response.json()
+    project_runs = response.json()
 
-    response = client.get(f"/instances/{instance_runs['instance_id']}/runs/last")
+    response = client.get(f"/projects/{project_runs['project_id']}/runs/last")
     assert response.status_code == 200
 
-    instance = response.json()
+    project = response.json()
 
-    assert instance["environment_slug"] == instance_runs["environment_slug"]
-    assert instance_runs["created_at"] == CREATED_TIME
+    assert project["environment_slug"] == project_runs["environment_slug"]
+    assert project_runs["created_at"] == CREATED_TIME
 
 
-def test_get_instance_runs(client: TestClient, reseed_tables) -> None:
-    """Test getting instance runs."""
-    response = client.get("/instances/1/runs/")
+def test_get_project_runs(client: TestClient, reseed_tables) -> None:
+    """Test getting project runs."""
+    response = client.get("/projects/1/runs/")
     assert response.status_code == 200
-    instances = response.json()
-    assert len(instances) == 1
+    projects = response.json()
+    assert len(projects) == 1
 
 
 def test_completed_run_status(
     session: sqlalchemy.orm.Session, reseed_tables, client: TestClient
 ) -> None:
     """Test completed run status."""
-    response = crud.update_instance_runs(
-        session, 1, schemas.InstanceRunsUpdate(completed_at=datetime.datetime.now())
+    response = crud.update_project_runs(
+        session, 1, schemas.ProjectRunsUpdate(completed_at=datetime.datetime.now())
     )
     assert response
     assert response.run_status == "completed"
@@ -50,8 +50,8 @@ def test_failed_run_status(
     session: sqlalchemy.orm.Session, reseed_tables, client: TestClient
 ) -> None:
     """Test failed run status."""
-    response = crud.update_instance_runs(
-        session, 2, schemas.InstanceRunsUpdate(failed_at=datetime.datetime.now())
+    response = crud.update_project_runs(
+        session, 2, schemas.ProjectRunsUpdate(failed_at=datetime.datetime.now())
     )
     assert response
     assert response.run_status == "failed"
@@ -61,8 +61,8 @@ def test_processing_run_status(
     session: sqlalchemy.orm.Session, reseed_tables, client: TestClient
 ) -> None:
     """Test prorcessing run status."""
-    response = crud.update_instance_runs(
-        session, 1, schemas.InstanceRunsUpdate(started_processing_at=datetime.datetime.now())
+    response = crud.update_project_runs(
+        session, 1, schemas.ProjectRunsUpdate(started_processing_at=datetime.datetime.now())
     )
     assert response
     assert response.run_status == "processing"
@@ -70,8 +70,8 @@ def test_processing_run_status(
 
 def test_in_queue_run_status(reseed_tables, client: TestClient) -> None:
     """Test in_queue run status."""
-    response = client.get("/instances/1/runs/?run_status=in_queue")
+    response = client.get("/projects/1/runs/?run_status=in_queue")
     assert response.status_code == 200
-    instance = response.json()
+    project = response.json()
 
-    assert instance[0]["run_status"] == "in_queue"
+    assert project[0]["run_status"] == "in_queue"
--- python/projects/phiphi/phiphi/tests/api/projects/test_projects.py
+++ python/projects/phiphi/phiphi/tests/api/projects/test_projects.py
@@ -1,15 +1,15 @@
-"""Test Instances."""
+"""Test Projects."""
 import pytest
 import sqlalchemy
 from fastapi.testclient import TestClient
 
-from phiphi.api.instances import models
+from phiphi.api.projects import models
 
 
-def test_instance_seeded(session: sqlalchemy.orm.Session, reseed_tables) -> None:
+def test_project_seeded(session: sqlalchemy.orm.Session, reseed_tables) -> None:
     """Test that the database is seeded."""
     response = session.execute(
-        sqlalchemy.select(sqlalchemy.func.count()).select_from(models.Instance)
+        sqlalchemy.select(sqlalchemy.func.count()).select_from(models.Project)
     )
     count = response.one()
     assert count
@@ -21,115 +21,115 @@ UPDATE_TIME = "2024-04-01T12:00:02"
 
 
 @pytest.mark.freeze_time(CREATED_TIME)
-def test_create_get_instance(reseed_tables, client: TestClient) -> None:
-    """Test create and then get of an instance."""
+def test_create_get_project(reseed_tables, client: TestClient) -> None:
+    """Test create and then get of an project."""
     data = {
-        "name": "first instance",
-        "description": "Instance 1",
+        "name": "first project",
+        "description": "Project 1",
         "environment_slug": "main",
         "pi_deleted_after_days": 90,
         "delete_after_days": 20,
         "expected_usage": "weekly",
     }
-    response = client.post("/instances/", json=data)
+    response = client.post("/projects/", json=data)
     assert response.status_code == 200
-    instance = response.json()
-    assert instance["name"] == data["name"]
-    assert instance["description"] == data["description"]
-    assert instance["environment_slug"] == data["environment_slug"]
-    assert instance["pi_deleted_after_days"] == data["pi_deleted_after_days"]
-    assert instance["delete_after_days"] == data["delete_after_days"]
-    assert instance["expected_usage"] == data["expected_usage"]
-    assert instance["created_at"] == CREATED_TIME
+    project = response.json()
+    assert project["name"] == data["name"]
+    assert project["description"] == data["description"]
+    assert project["environment_slug"] == data["environment_slug"]
+    assert project["pi_deleted_after_days"] == data["pi_deleted_after_days"]
+    assert project["delete_after_days"] == data["delete_after_days"]
+    assert project["expected_usage"] == data["expected_usage"]
+    assert project["created_at"] == CREATED_TIME
 
-    response = client.get(f"/instances/{instance['id']}")
+    response = client.get(f"/projects/{project['id']}")
     assert response.status_code == 200
 
-    instance = response.json()
+    project = response.json()
 
-    assert instance["name"] == data["name"]
-    assert instance["description"] == data["description"]
-    assert instance["created_at"] == CREATED_TIME
-    assert instance["environment_slug"] == data["environment_slug"]
-    assert instance["pi_deleted_after_days"] == data["pi_deleted_after_days"]
-    assert instance["delete_after_days"] == data["delete_after_days"]
+    assert project["name"] == data["name"]
+    assert project["description"] == data["description"]
+    assert project["created_at"] == CREATED_TIME
+    assert project["environment_slug"] == data["environment_slug"]
+    assert project["pi_deleted_after_days"] == data["pi_deleted_after_days"]
+    assert project["delete_after_days"] == data["delete_after_days"]
 
 
-def test_get_instance_not_found(client: TestClient, recreate_tables) -> None:
-    """Test getting an instance that does not exist."""
-    response = client.get("/instances/5")
+def test_get_project_not_found(client: TestClient, recreate_tables) -> None:
+    """Test getting an project that does not exist."""
+    response = client.get("/projects/5")
     assert response.status_code == 404
-    assert response.json() == {"detail": "Instance not found"}
+    assert response.json() == {"detail": "Project not found"}
 
 
-def test_get_instances(client: TestClient, reseed_tables) -> None:
-    """Test getting instances."""
-    response = client.get("/instances/")
+def test_get_projects(client: TestClient, reseed_tables) -> None:
+    """Test getting projects."""
+    response = client.get("/projects/")
     assert response.status_code == 200
-    instances = response.json()
-    assert len(instances) == 3
+    projects = response.json()
+    assert len(projects) == 3
 
 
-def test_get_instances_pagination(client: TestClient, reseed_tables) -> None:
+def test_get_projects_pagination(client: TestClient, reseed_tables) -> None:
     """Test getting users with pagination."""
-    response = client.get("/instances/?start=1&end=1")
+    response = client.get("/projects/?start=1&end=1")
     assert response.status_code == 200
-    instances = response.json()
-    assert len(instances) == 1
-    assert instances[0]["id"] == 2
+    projects = response.json()
+    assert len(projects) == 1
+    assert projects[0]["id"] == 2
 
 
 @pytest.mark.freeze_time(UPDATE_TIME)
-def test_update_instance(
+def test_update_project(
     client: TestClient, reseed_tables, session: sqlalchemy.orm.Session
 ) -> None:
-    """Test updating an instance."""
-    data = {"description": "New instance"}
-    instance_id = 1
-    response = client.put(f"/instances/{instance_id}", json=data)
+    """Test updating an project."""
+    data = {"description": "New project"}
+    project_id = 1
+    response = client.put(f"/projects/{project_id}", json=data)
     assert response.status_code == 200
-    instance = response.json()
-    assert instance["description"] == data["description"]
-    db_instance = session.get(models.Instance, instance_id)
-    assert db_instance
-    assert db_instance.description == data["description"]
-    assert db_instance.updated_at.isoformat() == UPDATE_TIME
+    project = response.json()
+    assert project["description"] == data["description"]
+    db_project = session.get(models.Project, project_id)
+    assert db_project
+    assert db_project.description == data["description"]
+    assert db_project.updated_at.isoformat() == UPDATE_TIME
 
 
-def test_update_instance_not_found(client: TestClient, recreate_tables) -> None:
-    """Test updating an instance that does not exist."""
-    data = {"description": "New instance"}
-    response = client.put("/instances/100", json=data)
+def test_update_project_not_found(client: TestClient, recreate_tables) -> None:
+    """Test updating an project that does not exist."""
+    data = {"description": "New project"}
+    response = client.put("/projects/100", json=data)
     assert response.status_code == 404
-    assert response.json() == {"detail": "Instance not found"}
+    assert response.json() == {"detail": "Project not found"}
 
 
 def test_environment_defaults_main(client: TestClient, reseed_tables) -> None:
     """Test that environment defaults to main, when nothing is passed as parameter."""
     data = {
-        "name": "first instance",
-        "description": "Instance 1",
+        "name": "first project",
+        "description": "Project 1",
         "pi_deleted_after_days": 90,
         "delete_after_days": 20,
         "expected_usage": "one_off",
     }
-    response = client.post("/instances/", json=data)
+    response = client.post("/projects/", json=data)
     assert response.status_code == 200
-    instance = response.json()
-    assert instance["environment_slug"] == "main"
+    project = response.json()
+    assert project["environment_slug"] == "main"
 
 
 @pytest.mark.freeze_time(CREATED_TIME)
-def test_create_instance_with_non_existing_env(recreate_tables, client: TestClient) -> None:
-    """Test create and then get of an instance."""
+def test_create_project_with_non_existing_env(recreate_tables, client: TestClient) -> None:
+    """Test create and then get of an project."""
     data = {
-        "name": "first instance",
-        "description": "Instance 1",
+        "name": "first project",
+        "description": "Project 1",
         "environment_slug": "non-existing",
         "pi_deleted_after_days": 90,
         "delete_after_days": 20,
         "expected_usage": "weekly",
     }
-    response = client.post("/instances/", json=data)
+    response = client.post("/projects/", json=data)
     assert response.status_code == 400
     assert response.json() == {"detail": "Environment not found"}
