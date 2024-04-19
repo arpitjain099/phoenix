39ce9edb001060eac57ae7d776b962dde0fbd3fb
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-04-19 10:18:19 +0200

    Rename instance to project for phiphi top level

--- python/projects/phiphi/phiphi/api/main.py
+++ python/projects/phiphi/phiphi/api/main.py
@@ -5,9 +5,9 @@ from starlette.middleware.cors import CORSMiddleware
 from phiphi import config
 from phiphi.api import health_check
 from phiphi.api.environments import routes as environment_routes
-from phiphi.api.instances import routes as instance_routes
-from phiphi.api.instances.gathers import routes as gather_routes
-from phiphi.api.instances.instance_runs import routes as instance_runs_routes
+from phiphi.api.projects import routes as project_routes
+from phiphi.api.projects.gathers import routes as gather_routes
+from phiphi.api.projects.project_runs import routes as project_runs_routes
 from phiphi.api.users import routes as user_routes
 
 app = FastAPI(title=config.settings.TITLE)
@@ -15,9 +15,9 @@ app = FastAPI(title=config.settings.TITLE)
 app.include_router(health_check.router)
 app.include_router(user_routes.router, tags=["User"])
 app.include_router(environment_routes.router, tags=["Environment"])
-app.include_router(instance_routes.router, tags=["Instance"])
-app.include_router(gather_routes.router, tags=["Instance Gathers"])
-app.include_router(instance_runs_routes.router, tags=["Instance Runs"])
+app.include_router(project_routes.router, tags=["Project"])
+app.include_router(gather_routes.router, tags=["Project Gathers"])
+app.include_router(project_runs_routes.router, tags=["Project Runs"])
 
 if config.settings.CORS_ORIGINS:
     app.add_middleware(
