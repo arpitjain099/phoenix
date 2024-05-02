8611f50e6e1e0a183887edc2e2020eebaeebc94b
Mba Gozpel <gosper4u@gmail.com>
2024-05-02 20:40:12 +0100

    removed create_apify and get _apify routes

--- python/projects/phiphi/phiphi/api/projects/gathers/routes.py
+++ python/projects/phiphi/phiphi/api/projects/gathers/routes.py
@@ -6,16 +6,6 @@ from phiphi.api.projects.gathers import crud, schemas
 router = fastapi.APIRouter()
 
 
-@router.post("/projects/{project_id}/gathers/apify", response_model=schemas.ApifyGatherResponse)
-def create_apify_gather(
-    gather: schemas.ApifyGatherCreate,
-    project_id: int,
-    session: deps.SessionDep,
-) -> schemas.ApifyGatherResponse:
-    """Create a new gather."""
-    return crud.create_apify_gather(session, project_id, gather)
-
-
 # REFACTORABLE: IF there are other gather subclasses, this should be refactored
 # to support polymorphic models and multiple return types
 @router.get("/projects/{project_id}/gathers/", response_model=list[schemas.GatherResponse])
@@ -27,24 +17,14 @@ def get_gathers(
 
 
 @router.get(
-    "/projects/{project_id}/gathers/apify/{apify_gather_id}",
+    "/projects/{project_id}/gathers/{gather_id}",
     response_model=schemas.ApifyGatherResponse,
 )
-def get_apify_gather(
+def get_gather(
     project_id: int, apify_gather_id: int, session: deps.SessionDep
-) -> schemas.ApifyGatherResponse:
+) -> schemas.GatherResponse:
     """Get an apify gather."""
-    apify_gather = crud.get_apify_gather(session, project_id, apify_gather_id)
+    apify_gather = crud.get_gather(session, project_id, apify_gather_id)
     if apify_gather is None:
         raise fastapi.HTTPException(status_code=404, detail="Gather not found")
     return apify_gather
-
-
-@router.get(
-    "/projects/{project_id}/gathers/apify", response_model=list[schemas.ApifyGatherResponse]
-)
-def get_apify_gathers(
-    session: deps.SessionDep, project_id: int, start: int = 0, end: int = 100
-) -> list[schemas.ApifyGatherResponse]:
-    """Get apify gathers."""
-    return crud.get_apify_gathers(session, project_id, start, end)
