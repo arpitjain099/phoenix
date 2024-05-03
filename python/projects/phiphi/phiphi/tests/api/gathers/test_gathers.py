c891fa82d7893254af6afbdaa5b5de454e3b1acf
Mba Gozpel <gosper4u@gmail.com>
2024-05-03 09:58:09 +0100

    add tests for apify_facebook_post_gather

--- python/projects/phiphi/phiphi/tests/api/gathers/test_gathers.py
+++ python/projects/phiphi/phiphi/tests/api/gathers/test_gathers.py
@@ -1,32 +1,28 @@
 """Test Gathers."""
 from fastapi.testclient import TestClient
 
-CREATED_TIME = "2024-04-01T12:00:01"
-UPDATE_TIME = "2024-04-01T12:00:02"
 
-# Commenting this out because it will fail since there is no create_gather implementation
-# Will refactor it in the next tasks
-# def test_get_gather(client: TestClient, reseed_tables) -> None:
-#     """Test getting gathers."""
-#     response = client.get("/projects/1/gathers/1")
-#     assert response.status_code == 200
-#     gather_1 = response.json()
+def test_get_gather(client: TestClient, reseed_tables) -> None:
+    """Test getting gathers."""
+    response = client.get("/projects/1/gathers/1")
+    assert response.status_code == 200
+    gather_1 = response.json()
 
-#     response_2 = client.get("/projects/2/gathers/3")
-#     assert response_2.status_code == 200
-#     gather_2 = response_2.json()
-#     assert gather_1["description"] != gather_2["description"]
+    response_2 = client.get("/projects/2/gathers/3")
+    assert response_2.status_code == 200
+    gather_2 = response_2.json()
+    assert gather_1["description"] != gather_2["description"]
 
-#     assert gather_1["id"] == 1
-#     assert gather_1["project_id"] == 1
-#     assert gather_2["id"] == 3
-#     assert gather_2["project_id"] == 2
+    assert gather_1["id"] == 1
+    assert gather_1["project_id"] == 1
+    assert gather_2["id"] == 3
+    assert gather_2["project_id"] == 2
 
-#     # Check that it is not always the first gather that is gotten
-#     response_3 = client.get("/projects/1/gathers/2")
-#     gather_3 = response_3.json()
-#     assert gather_3["id"] == 2
-#     assert gather_3["project_id"] == 1
+    # Check that it is not always the first gather that is gotten
+    response_3 = client.get("/projects/1/gathers/2")
+    gather_3 = response_3.json()
+    assert gather_3["id"] == 2
+    assert gather_3["project_id"] == 1
 
 
 def test_get_gathers(client: TestClient, reseed_tables) -> None:
@@ -34,9 +30,9 @@ def test_get_gathers(client: TestClient, reseed_tables) -> None:
     response = client.get("/projects/1/gathers/")
     assert response.status_code == 200
     gathers = response.json()
-    assert len(gathers) == 0
+    assert len(gathers) == 2
 
     response = client.get("/projects/2/gathers/")
     assert response.status_code == 200
     gathers = response.json()
-    assert len(gathers) == 0
+    assert len(gathers) == 1
