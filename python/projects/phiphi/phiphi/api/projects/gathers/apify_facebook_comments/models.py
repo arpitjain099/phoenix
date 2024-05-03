2f06c21f4ff96a4228fb2f43b6c97766d259999e
Mba Gozpel <gosper4u@gmail.com>
2024-05-03 15:28:22 +0100

    created models, schemas migration revision for apify_facebook_comments

---
+++ python/projects/phiphi/phiphi/api/projects/gathers/apify_facebook_comments/models.py
@@ -0,0 +1,20 @@
+"""Apify facebook comments gather model."""
+
+from phiphi.api import base_models
+from phiphi.api.projects.gathers import models as gather_models
+from sqlalchemy import ForeignKey, orm
+
+
+class ApifyFacebookCommentGather(gather_models.Gather):
+    """Apify Gather model."""
+
+    __tablename__ = "apify_facebook_comment_gathers"
+    __mapper_args__ = {
+        "polymorphic_identity": "apify_facebook_comments",
+    }
+
+    id: orm.Mapped[int] = orm.mapped_column(ForeignKey("gathers.id"), primary_key=True)
+    limit_comments_per_post: orm.Mapped[int]
+    post_url_list: orm.Mapped[str] = orm.mapped_column(base_models.JSONEncodedValue)
+    sort_comments_by: orm.Mapped[str]
+    include_comment_replies: orm.Mapped[bool]
---
+++ python/projects/phiphi/phiphi/api/projects/gathers/apify_facebook_comments/schemas.py
@@ -0,0 +1,65 @@
+"""Schemas for apify facebook comments gathers."""
+
+from enum import Enum
+from typing import Annotated
+
+import pydantic
+from phiphi.api.projects.gathers import schemas as gather_schemas
+
+
+class SortComment(str, Enum):
+    """SortComment enum."""
+
+    facebook_default = "facebook_default"
+    most_relevant = "most_relevant"
+    newest_first = "newest_first"
+    non_filtered = "non_filtered"
+
+
+class ApifyFacebookCommentGatherBase(gather_schemas.GatherBase):
+    """Apify Facebook Comments Gather config schema."""
+
+    post_url_list: Annotated[
+        list[str],
+        pydantic.Field(description="The post url list, should be the full url including https"),
+    ]
+
+    limit_comments_per_post: Annotated[
+        int,
+        pydantic.Field(
+            default=1000,
+            description="",
+        ),
+    ]
+    sort_comments_by: Annotated[
+        SortComment,
+        pydantic.Field(description="", default=SortComment.facebook_default),
+    ]
+    include_comment_replies: Annotated[
+        bool,
+        pydantic.Field(
+            description="",
+        ),
+    ]
+
+
+class ApifyFacebookCommentGatherResponse(
+    gather_schemas.GatherResponse, ApifyFacebookCommentGatherBase
+):
+    """Apify Facebook Comments Gather schema.
+
+    Properties to return to client.
+    """
+
+    model_config = pydantic.ConfigDict(from_attributes=True)
+
+
+class ApifyFacebookCommentGatherCreate(ApifyFacebookCommentGatherBase):
+    """Apify Facebook Comments  Gather create schema.
+
+    Properties to receive via API on creation.
+    """
+
+
+class ApifyFacebookCommentGatherUpdate(gather_schemas.GatherUpdate):
+    """Apify Facebook Comments  Gather update schema."""
