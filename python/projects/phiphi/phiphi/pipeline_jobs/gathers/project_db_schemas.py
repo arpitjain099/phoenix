418bf1a68c6c38beb15c4e1325e3b85a0337d1e2
dbcerigo <dan@datavaluepeople.com>
2024-05-09 16:54:13 +0900

    Phiphi: implement generalised messages schema
    Notes:
    - Removed `id` as soft assumption we will not be using postgres/for
      analytics you don't really ever want to do row look ups anyway.
    - Added note on the columns which make up using messages, and unique
      message version (e.g. latest messages), so we are ready to write the
      query that produces `latest_generalised_messages`.
    - Again, soft assuming not postgres, then we can't do the parent child
      tables, so I think we just duplicate the cols for unique message
      version into each of the platform-data_type specific tables that
      contain their un-generalised data.

---
+++ python/projects/phiphi/phiphi/pipeline_jobs/gathers/project_db_schemas.py
@@ -0,0 +1,41 @@
+"""Tables schemas for the project database."""
+import pandera as pa
+
+# Schema that all messages (of any type and any source) should be normalised to
+generalised_messages_schema = pa.DataFrameSchema(
+    {
+        "project_id": pa.Column(pa.Int, nullable=False),
+        "gather_id": pa.Column(pa.Int, nullable=False),
+        "gather_batch_id": pa.Column(pa.Int, nullable=False),
+        "gathered_at": pa.Column(pa.DateTime, nullable=False),
+        "phoenix_processed_at": pa.Column(pa.DateTime, nullable=False),
+        "source": pa.Column(pa.String, checks=pa.Check.isin(["apify"]), nullable=False),
+        # NOTE: combination of (platform, data_type, pi_platform_message_id) should give a unique
+        # message, but it won't be a unique row as we process the same message multiple times.
+        # Doing groupby on these columns then get MAX(phoenix_processed_at) will give the latest
+        # message.
+        # `phoenix_platform_message_id` can be used instead of `pi_platform_message_id` also.
+        "platform": pa.Column(
+            pa.String,
+            checks=pa.Check.isin(["facebook", "instagram", "tiktok", "x-twitter"]),
+            nullable=False,
+        ),
+        "data_type": pa.Column(
+            pa.String, checks=pa.Check.isin(["post", "comment"]), nullable=False
+        ),
+        "pi_platform_message_id": pa.Column(nullable=True),
+        "pi_platform_message_author_id": pa.Column(nullable=True),
+        "pi_platform_message_author_name": pa.Column(pa.String, nullable=True),
+        "pi_platform_parent_message_id": pa.Column(nullable=True),
+        "pi_text": pa.Column(pa.String, nullable=True),
+        "pi_platform_message_url": pa.Column(pa.String, nullable=True),
+        "platform_message_last_updated_at": pa.Column(pa.DateTime, nullable=False),
+        # Hash of `pi_platform_message_id`.
+        "phoenix_platform_message_id": pa.Column(pa.String, nullable=False),
+        # Hash of `pi_platform_message_author_id`.
+        "phoenix_platform_message_author_id": pa.Column(pa.String, nullable=False),
+        # Note, no version of the author name, as the id serves as the non-pi identifier.
+        # Hash of `pi_platform_parent_message_id`.
+        "phoenix_platform_parent_message_id": pa.Column(pa.String, nullable=True),
+    }
+)
