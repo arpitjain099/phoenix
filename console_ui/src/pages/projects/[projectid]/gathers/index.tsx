1f9f4dce713d6ecbaca6e480b9aa4f9813556c7f
Chisom Marshall Ogbuagu <36017222+chiysom@users.noreply.github.com>
2024-05-15 11:14:48 +0100

    Gather list updated

--- console_ui/src/pages/projects/[projectid]/gathers/index.tsx
+++ console_ui/src/pages/projects/[projectid]/gathers/index.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import {
 	IResourceComponentsProps,
 	useTranslate,
-	GetManyResponse,
 	useMany,
 	useList,
 } from "@refinedev/core";
 import { useTable } from "@refinedev/react-table";
 import { ColumnDef } from "@tanstack/react-table";
-import { ScrollArea, Pagination } from "@mantine/core";
+import { ScrollArea, Pagination, Group, Button } from "@mantine/core";
 import { List, DateField } from "@refinedev/mantine";
 import TableComponent from "@components/table";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import BreadcrumbsComponent from "@components/breadcrumbs";
+import { statusTextStyle } from "src/utils";
+import { IconPlayerPlay, IconRefresh, IconTrash } from "@tabler/icons";
 
 export const GatherList: React.FC<IResourceComponentsProps> = () => {
 	const translate = useTranslate();
@@ -32,6 +32,11 @@ export const GatherList: React.FC<IResourceComponentsProps> = () => {
 
 	const columns = React.useMemo<ColumnDef<any>[]>(
 		() => [
+			{
+				id: "source",
+				accessorKey: "source",
+				header: translate("gathers.fields.source"),
+			},
 			{
 				id: "platform",
 				accessorKey: "platform",
@@ -56,46 +61,52 @@ export const GatherList: React.FC<IResourceComponentsProps> = () => {
 				},
 			},
 			{
-				id: "only_posts_newer_than",
-				accessorKey: "only_posts_newer_than",
-				header: translate("gathers.fields.only_posts_newer_than"),
+				id: "run_status",
+				accessorKey: "run_status",
+				header: translate("projects.fields.status"),
 				cell: function render({ getValue }) {
-					return <DateField value={getValue<any>()} />;
-				},
-			},
-			{
-				id: "project_id",
-				header: translate("gathers.fields.project_id"),
-				accessorKey: "project_id",
-				cell: function render({ getValue, table }) {
-					const meta = table.options.meta as {
-						projectData: GetManyResponse;
-					};
-
-					const project = meta.projectData?.data?.find(
-						(item) => item.id === getValue<any>()
-					);
-
-					return project ? (
-						<Link
-							href={{
-								pathname: "/projects/show/[projectid]",
-								query: { projectid: project?.id },
-							}}
-						>
-							{project?.name}
-						</Link>
-					) : (
-						"Loading..."
+					return (
+						<span
+							className={`${statusTextStyle(getValue())}`}
+						>{`${getValue()}`}</span>
 					);
 				},
 			},
 			{
-				id: "created_at",
-				accessorKey: "created_at",
-				header: translate("gathers.fields.created_at"),
-				cell: function render({ getValue }) {
-					return <DateField value={getValue<any>()} />;
+				id: "actions",
+				accessorKey: "id",
+				header: translate("table.actions"),
+				cell: function render({ row }) {
+					const { run_status } = row.original;
+					return (
+						<Group spacing="xs" noWrap>
+							{run_status === "in_queue" && (
+								<Button p={0} variant="subtle" onClick={() => {}}>
+									<IconRefresh size={20} color="blue" />
+								</Button>
+							)}
+							{run_status === "processing" && (
+								<Button p={0} variant="subtle" onClick={() => {}}>
+									<IconRefresh size={20} color="blue" />
+								</Button>
+							)}
+							{run_status === "not_yet_run" && (
+								<Button p={0} variant="subtle" color="green" onClick={() => {}}>
+									<IconPlayerPlay size={20} color="green" />
+								</Button>
+							)}
+							{run_status === "failed" && (
+								<Button p={0} variant="subtle" color="red" onClick={() => {}}>
+									<IconTrash size={20} color="red" className="cursor-pointer" />
+								</Button>
+							)}
+							{run_status === "completed" && (
+								<Button p={0} variant="subtle" color="red" onClick={() => {}}>
+									<IconTrash size={20} color="red" className="cursor-pointer" />
+								</Button>
+							)}
+						</Group>
+					);
 				},
 			},
 		],
--- console_ui/src/utils/index.ts
+++ console_ui/src/utils/index.ts
@@ -8,3 +8,21 @@ export const toReadableDate = (date: string): string => {
 	const fd = new Date(date);
 	return fd.toDateString();
 };
+
+export const statusTextStyle = (type: any): string => {
+  switch (type) {
+    case "in_queue":
+      return "text-orange-500";
+		case "not_yet_run":
+			return "text-orange-500";
+    case "processing":
+      return "text-orange-500";
+    case "failed":
+      return "text-red-500";
+    case "completed":
+      return "text-green-500";
+    default:
+      return "";
+  }
+}
+
