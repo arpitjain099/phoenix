be725b6615450361668386b535e3386c1d7f04ab
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-05-17 15:18:08 +0200

    Add POST /projects/{project_id}/job_runs/

--- python/projects/phiphi/phiphi/api/projects/job_runs/routes.py
+++ python/projects/phiphi/phiphi/api/projects/job_runs/routes.py
@@ -1,4 +1,14 @@
 """JobRun routes."""
 import fastapi
+from phiphi.api import deps
+from phiphi.api.projects.job_runs import crud, schemas
 
 router = fastapi.APIRouter()
+
+
+@router.post("/projects/{project_id}/job_runs/", response_model=schemas.JobRunResponse)
+def create_job_run(
+    project_id: int, session: deps.SessionDep, job_run_create: schemas.JobRunCreate
+) -> schemas.JobRunResponse:
+    """Create a Project Job Run."""
+    return crud.create_job_run(session, project_id, job_run_create)
---
+++ python/projects/phiphi/phiphi/tests/api/projects/test_job_runs.py
@@ -0,0 +1,26 @@
+"""Test job runs routes."""
+import pytest
+from fastapi.testclient import TestClient
+
+from phiphi.api.projects.job_runs import schemas
+
+CREATED_TIME = "2024-04-01T12:00:01"
+UPDATE_TIME = "2024-04-01T12:00:02"
+
+
+@pytest.mark.freeze_time(CREATED_TIME)
+def test_create_get_job_runs(reseed_tables, client: TestClient) -> None:
+    """Test create and then get of an job run."""
+    data = {
+        "foreign_id": 1,
+        "foreign_job_type": "gather",
+    }
+
+    response = client.post("/projects/1/job_runs/", json=data)
+    assert response.status_code == 200
+    job_run = response.json()
+    assert job_run["foreign_id"] == data["foreign_id"]
+    assert job_run["foreign_job_type"] == data["foreign_job_type"]
+    assert job_run["created_at"] == CREATED_TIME
+    assert job_run["project_id"] == 1
+    assert job_run["status"] == schemas.Status.awaiting_start
