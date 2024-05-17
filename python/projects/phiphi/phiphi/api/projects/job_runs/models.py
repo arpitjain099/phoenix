5b7809ef57444ecbf03a773fb5ff7c6c1f7451f5
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-05-17 12:03:24 +0200

    Add project_id to job_runs model
    Includes the addition of one index.
    
    See thoughts from chart gpt on multi indexes:
    https://chatgpt.com/share/865f9306-2c31-4a0d-b5a0-26a768ba334e

--- python/projects/phiphi/phiphi/api/projects/job_runs/models.py
+++ python/projects/phiphi/phiphi/api/projects/job_runs/models.py
@@ -15,9 +15,25 @@ class JobRuns(platform_db.Base):
     """Job runs model."""
 
     __tablename__ = "job_runs"
-    __table_args__ = (Index("idx_foreign_job_type_foreign_id", "foreign_job_type", "foreign_id"),)
+    __table_args__ = (
+        # Requests with `WHERE project_id` will also use this index.
+        Index(
+            "idx_project_id_foreign_job_type_foreign_id",
+            "project_id",
+            "foreign_job_type",
+            "foreign_id",
+        ),
+        # This index is added to make sure that the queries with two columns as there is a high
+        # chance that the queries will not use `idx_project_id_foreign_job_type_foreign_id` index.
+        Index(
+            "idx_foreign_job_type_foreign_id",
+            "foreign_job_type",
+            "foreign_id",
+        ),
+    )
 
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    project_id: orm.Mapped[int]
     row_created_at: orm.Mapped[datetime.datetime]
     foreign_id: orm.Mapped[int]
     foreign_job_type: orm.Mapped[str]
