34e73e1fdbcbc5048c7ffcb8f97e0c3b44b1526a
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-05-21 15:03:43 +0200

    Refactor hello flows in to dummy health check flow

--- python/projects/phiphi/phiphi/health_check_flows.py
+++ python/projects/phiphi/phiphi/health_check_flows.py
@@ -1,16 +1,20 @@
 """Health check flows."""
+import logging
 from typing import Coroutine
 
 import prefect
 
-from phiphi import constants
+from phiphi import config, constants
+
+logger = logging.getLogger(__name__)
 
 
 @prefect.flow
-def hello(hello_name: str = "Benjamin") -> None:
-    """Prints a greeting to the provided name."""
-    logger = prefect.get_run_logger()
-    logger.info(f"Hello, {hello_name}!")
+def health_check(environment_slug: str | None) -> None:
+    """Main flow for the health check."""
+    logger.info("Health checks started.")
+    assert True
+    logger.info("Health checks completed.")
 
 
 def create_deployments(
@@ -21,9 +25,7 @@ def create_deployments(
     build: bool = False,
     push: bool = False,
 ) -> list[Coroutine]:
-    """Create deployments for hello world flows.
-
-    Creates the default and john deployments for the hello world flow.
+    """Create deployments for health check flows.
 
     By default the deployments are into the main work pool.
 
@@ -43,17 +45,17 @@ def create_deployments(
     work_pool_name = str(constants.WorkPool.main)
     if override_work_pool_name:
         work_pool_name = override_work_pool_name
-    names = ["default", "john"]
+    environ_slugs = [config.settings.FIRST_ENVIRONMENT_SLUG]
     coroutines = []
-    for name in names:
-        task = hello.deploy(
-            name=deployment_name_prefix + name,
+    for slug in environ_slugs:
+        task = health_check.deploy(
+            name=deployment_name_prefix + slug,
             work_pool_name=work_pool_name,
             image=image,
             build=build,
             push=push,
             tags=tags,
-            parameters={"hello_name": name},
+            parameters={"environment_slug": slug},
         )
         coroutines.append(task)
 
