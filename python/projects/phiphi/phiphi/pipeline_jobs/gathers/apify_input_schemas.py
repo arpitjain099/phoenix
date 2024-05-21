c7afe350c8f5ce5e20a996e08254a664be1fe4c8
dbcerigo <dan@datavaluepeople.com>
2024-05-21 17:28:17 +0900

    Phiphi: implement tiktok comments gather input

--- python/projects/phiphi/phiphi/pipeline_jobs/gathers/apify_input_schemas.py
+++ python/projects/phiphi/phiphi/pipeline_jobs/gathers/apify_input_schemas.py
@@ -168,3 +168,38 @@ class TikTokScraperInput(pydantic.BaseModel):
         """Pydantic configuration."""
 
         extra = pydantic.Extra.forbid
+
+
+class TikTokCommentsScraperInput(pydantic.BaseModel):
+    """Input schema for the TikTok Comments Scraper.
+
+    Facilitates the extraction of comments from specific TikTok videos by providing URLs.
+
+    Ref to relevant Apify actor docs: https://apify.com/clockworks/tiktok-scraper/input-schema
+    """
+
+    post_urls: List[UrlStr] = pydantic.Field(
+        ...,
+        serialization_alias="postURLs",
+        description="List of URLs for TikTok videos from which to scrape comments.",
+    )
+    comments_per_post: int = pydantic.Field(
+        25,
+        serialization_alias="commentsPerPost",
+        description=(
+            "Maximum number of comments to scrape from each video; defaults to 100. "
+            "Over 500 can be slow."
+        ),
+    )
+    max_replies_per_comment: int = pydantic.Field(
+        0,
+        serialization_alias="maxRepliesPerComment",
+        description="Docs say 'Slow'. Maximum number of replies to scrape from each comment; "
+        "defaults to 0. NOTE: it is currently not guaranteed that the scraper will manage to "
+        "scrape ALL the desired replies. By default no replies are scraped.",
+    )
+
+    class Config:
+        """Pydantic configuration."""
+
+        extra = pydantic.Extra.forbid
