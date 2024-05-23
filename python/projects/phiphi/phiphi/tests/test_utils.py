44071cb710a9b464b1003e1c3397698e2416082c
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-05-23 09:22:09 +0200

    Refactor get_apify_key tests to improve names

--- python/projects/phiphi/phiphi/tests/test_utils.py
+++ python/projects/phiphi/phiphi/tests/test_utils.py
@@ -3,27 +3,38 @@ import pytest
 
 from phiphi import utils
 
-APIFY_API_KEYS_STR_1 = '{"main": "main_test", "test": "test_test"}'
-SETTINGS_ENV_MAIN = {"APIFY_API_KEYS": APIFY_API_KEYS_STR_1, "FIRST_ENVIRONMENT_SLUG": "main"}
-SETTINGS_ENV_TEST = {"APIFY_API_KEYS": APIFY_API_KEYS_STR_1, "FIRST_ENVIRONMENT_SLUG": "test"}
+MAIN_APIFY_API_KEY = "main_apify_key"
+OTHER_APIFY_API_KEY = "other_apify_key"
+OTHER_ENV_SLUG = "other_env_slug"
+APIFY_API_KEYS_STR_1 = (
+    f'{{"main_env_slug": "{MAIN_APIFY_API_KEY}", "{OTHER_ENV_SLUG}": "{OTHER_APIFY_API_KEY}"}}'
+)
+SETTINGS_ENV_MAIN = {
+    "APIFY_API_KEYS": APIFY_API_KEYS_STR_1,
+    "FIRST_ENVIRONMENT_SLUG": "main_env_slug",
+}
+SETTINGS_ENV_OTHER = {
+    "APIFY_API_KEYS": APIFY_API_KEYS_STR_1,
+    "FIRST_ENVIRONMENT_SLUG": OTHER_ENV_SLUG,
+}
 
 
 @pytest.mark.patch_settings(SETTINGS_ENV_MAIN)
 def test_get_apify_keys_main(patch_settings):
     """Test get apify keys."""
-    assert utils.get_apify_api_key() == "main_test"
+    assert utils.get_apify_api_key() == MAIN_APIFY_API_KEY
 
 
 @pytest.mark.patch_settings(SETTINGS_ENV_MAIN)
-def test_get_apify_keys_test(patch_settings):
-    """Test get apify keys."""
-    assert utils.get_apify_api_key("test") == "test_test"
+def test_get_apify_keys_other(patch_settings):
+    """Test get apify keys for other env."""
+    assert utils.get_apify_api_key(OTHER_ENV_SLUG) == OTHER_APIFY_API_KEY
 
 
-@pytest.mark.patch_settings(SETTINGS_ENV_TEST)
-def test_get_apify_keys_test_env(patch_settings):
+@pytest.mark.patch_settings(SETTINGS_ENV_OTHER)
+def test_get_apify_keys_other_env(patch_settings):
     """Test get apify keys."""
-    assert utils.get_apify_api_key() == "test_test"
+    assert utils.get_apify_api_key() == OTHER_APIFY_API_KEY
 
 
 @pytest.mark.patch_settings(SETTINGS_ENV_MAIN)
