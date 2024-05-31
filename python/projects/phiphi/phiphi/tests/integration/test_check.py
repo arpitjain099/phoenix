8143d53fcb2cc37b16f2f3ea688a04c2c352f872
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-05-31 14:28:44 +0200

    Add integration tests to phiphi
    These tests are ignored by default but can be run with:
    `pytest tests/integration`. Running this will currently fail.

---
+++ python/projects/phiphi/phiphi/tests/integration/test_check.py
@@ -0,0 +1,6 @@
+"""This is a test file for the check module."""
+
+
+def test_integration_folder(session):
+    """Temporary test to make sure that the integration folder is not run."""
+    assert False
