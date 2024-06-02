2a9b2f13e57f9478d86e351b4abafb77716ca7d8
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-06-02 08:36:32 +0200

    Refactor test_bq_data_pipeline to stop conflicts between runs

--- python/projects/phiphi/phiphi/tests/integration/test_bq_data_pipeline.py
+++ python/projects/phiphi/phiphi/tests/integration/test_bq_data_pipeline.py
@@ -1,11 +1,22 @@
 """Integration tests for the data pipeline with big query."""
+import uuid
+
 from google.cloud import bigquery
 
 from phiphi.pipeline_jobs import projects
 
 
 def test_bq_pipeline_integration(session_context, reseed_tables):
-    """Test pipeline integration with bigquery."""
-    dataset = projects.init_project_db.fn(project_id=-1)
+    """Test pipeline integration with bigquery.
+
+    This creates a dataset with prefect `test_<random_prefix>`.
+
+    If the test fails you may need to manually clean up the dataset.
+    """
+    maxval = 10
+    temp_prefix = str(uuid.uuid4())[:maxval]
+    temp_prefix = temp_prefix.replace("-", "")
+    test_prefix = f"test_{temp_prefix}"
+    dataset = projects.init_project_db.fn(project_id=1, namespace_prefix=test_prefix)
     client = bigquery.Client()
     assert client.get_dataset(dataset)
