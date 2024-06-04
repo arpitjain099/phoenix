ac49527539eb2f4c3cc036ecc83ce84444b7725a
dbcerigo <dan@datavaluepeople.com>
2024-06-04 21:08:20 +0900

    Phiphi: formatting

--- python/projects/phiphi/phiphi/api/projects/job_runs/flow_runner_flow.py
+++ python/projects/phiphi/phiphi/api/projects/job_runs/flow_runner_flow.py
@@ -7,7 +7,12 @@ from prefect.client.schemas import objects
 from prefect.deployments import deployments
 from prefect.flow_runs import wait_for_flow_run
 
-from phiphi import all_platform_models, constants, platform_db, utils  # noqa: F401
+from phiphi import (
+    all_platform_models,  # noqa: F401
+    constants,
+    platform_db,
+    utils,
+)
 from phiphi.api.projects import gathers, job_runs
 from phiphi.types import PhiphiJobType
 
