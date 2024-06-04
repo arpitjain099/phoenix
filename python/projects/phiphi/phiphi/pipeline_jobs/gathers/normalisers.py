5e05951576fb315b94ac5f17760865053562c2e8
dbcerigo <dan@datavaluepeople.com>
2024-06-04 20:07:43 +0900

    Phiphi: gather normalise - minor clean up

--- python/projects/phiphi/phiphi/pipeline_jobs/gathers/normalisers.py
+++ python/projects/phiphi/phiphi/pipeline_jobs/gathers/normalisers.py
@@ -12,7 +12,6 @@ def anonymize(input_value: Union[str, int]) -> str:
 
 def normalise_single_facebook_posts_json(json_blob: Dict) -> Dict:
     """Extract fields from a single Facebook post JSON blob to normalized form."""
-    platform_message_last_updated_at = datetime.fromisoformat(json_blob["time"][:-1])
     return {
         "pi_platform_message_id": json_blob["postId"],
         "pi_platform_message_author_id": json_blob["user"]["id"],
@@ -20,7 +19,7 @@ def normalise_single_facebook_posts_json(json_blob: Dict) -> Dict:
         "pi_platform_parent_message_id": None,  # Posts don't have parent messages
         "pi_text": json_blob["text"],
         "pi_platform_message_url": json_blob["url"],
-        "platform_message_last_updated_at": platform_message_last_updated_at,
+        "platform_message_last_updated_at": datetime.fromisoformat(json_blob["time"]),
         "phoenix_platform_message_id": anonymize(json_blob["postId"]),
         "phoenix_platform_message_author_id": anonymize(json_blob["user"]["id"]),
         "phoenix_platform_parent_message_id": None,  # Posts don't have parent messages
