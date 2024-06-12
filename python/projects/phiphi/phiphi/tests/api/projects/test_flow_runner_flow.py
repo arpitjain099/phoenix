faf22beccb719af362d3a52a62e68c05076c555d
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-06-12 10:14:05 +0200

    Add tabulate to flow_runner_flow

--- python/projects/phiphi/phiphi/tests/api/projects/test_flow_runner_flow.py
+++ python/projects/phiphi/phiphi/tests/api/projects/test_flow_runner_flow.py
@@ -27,6 +27,13 @@ from phiphi.api.projects.job_runs import crud, flow_runner_flow, schemas
             False,
             schemas.Status.failed,
         ),
+        (
+            0,
+            schemas.ForeignJobType.tabulate,
+            "tabulate_flow/tabulate_flow",
+            True,
+            schemas.Status.completed_sucessfully,
+        ),
     ],
 )
 @mock.patch("prefect.flow_runs.wait_for_flow_run", new_callable=AsyncMock)
