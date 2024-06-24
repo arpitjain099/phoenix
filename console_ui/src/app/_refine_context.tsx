f729e08e3840c429b101ab4fd258c874dbc295e9
Chisom Marshall Ogbuagu <36017222+chiysom@users.noreply.github.com>
2024-06-24 08:35:17 +0100

    Miigrating from pages to app

---
+++ console_ui/src/app/_refine_context.tsx
@@ -0,0 +1,153 @@
+"use client";
+
+import { Authenticated, CanAccess, Refine } from "@refinedev/core";
+import { DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import {
+	RefineThemes,
+	ThemedLayoutV2,
+	ThemedTitleV2,
+	useNotificationProvider,
+} from "@refinedev/mantine";
+import routerProvider from "@refinedev/nextjs-router";
+import React, { PropsWithChildren } from "react";
+import "@styles/global.css";
+
+import {
+	ColorScheme,
+	ColorSchemeProvider,
+	Global,
+	MantineProvider,
+} from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
+import { NotificationsProvider } from "@mantine/notifications";
+import dataProvider from "@providers/data-provider";
+import { useTranslation } from "next-i18next";
+
+// initialize i18n
+import "../providers/i18n";
+import { IconUser } from "@tabler/icons";
+import authProvider from "@providers/auth-provider";
+import accessControlProvider from "@providers/access-provider";
+import Header from "@components/header";
+import Image from "next/image";
+
+const RefineContext = ({ children }: PropsWithChildren) => {
+	const { t, i18n } = useTranslation();
+
+	const i18nProvider = {
+		translate: (key: string, params: object) => t(key, params),
+		changeLocale: (lang: string) => i18n.changeLanguage(lang),
+		getLocale: () => i18n.language,
+	};
+
+	const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+		key: "mantine-color-scheme",
+		defaultValue: "light",
+		getInitialValueInEffect: true,
+	});
+	const toggleColorScheme = (value?: ColorScheme) =>
+		setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+	return (
+		<RefineKbarProvider>
+			<ColorSchemeProvider
+				colorScheme={colorScheme}
+				toggleColorScheme={toggleColorScheme}
+			>
+				{/* You can change the theme colors here. example: theme={{ ...RefineThemes.Magenta, colorScheme:colorScheme }} */}
+				<MantineProvider
+					theme={{ ...RefineThemes.Blue, colorScheme }}
+					withNormalizeCSS
+					withGlobalStyles
+				>
+					<Global styles={{ body: { WebkitFontSmoothing: "auto" } }} />
+					<NotificationsProvider position="top-right">
+						<DevtoolsProvider>
+							<Refine
+								routerProvider={routerProvider}
+								dataProvider={dataProvider}
+								notificationProvider={useNotificationProvider}
+								i18nProvider={i18nProvider}
+								authProvider={authProvider}
+								accessControlProvider={accessControlProvider}
+								resources={[
+									{
+										name: "projects",
+										list: "/projects",
+										create: "/projects/create",
+										edit: "/projects/edit/:id",
+										show: "/projects/show/:id",
+										meta: {
+											canDelete: true,
+										},
+									},
+									{
+										name: "",
+										list: "/projects/:projectid/gathers",
+										create: "/projects/:projectid/gathers/create",
+										meta: {
+											label: "Gathers",
+											parent: "projects",
+											hide: true,
+										},
+									},
+									{
+										name: "",
+										list: "/projects/:projectid/tabulate",
+										meta: {
+											label: t("tabulate.title"),
+											parent: "projects",
+											hide: true,
+										},
+									},
+									{
+										name: "profile",
+										list: "/profile",
+										meta: {
+											label: "Profile",
+											icon: <IconUser size="16" />,
+										},
+									},
+								]}
+								options={{
+									syncWithLocation: true,
+									warnWhenUnsavedChanges: true,
+									useNewQueryKeys: true,
+									projectId: "nMl5vA-MzwgF4-ONcYky",
+								}}
+							>
+								<Authenticated key="app">
+									<CanAccess>
+										<ThemedLayoutV2
+											Header={() => <Header sticky />}
+											Title={({ collapsed }) => (
+												<ThemedTitleV2
+													collapsed={collapsed}
+													text="Console - Phoenix"
+													icon={
+														<Image
+															src="/logo_buildup.png"
+															width={32}
+															height={32}
+															alt="logo"
+														/>
+													}
+												/>
+											)}
+										>
+											{children}
+										</ThemedLayoutV2>
+									</CanAccess>
+								</Authenticated>
+								<RefineKbar />
+							</Refine>
+						</DevtoolsProvider>
+					</NotificationsProvider>
+				</MantineProvider>
+			</ColorSchemeProvider>
+		</RefineKbarProvider>
+	);
+};
+
+export default RefineContext;
---
+++ console_ui/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import React, { Suspense } from "react";
+import { Metadata } from "next";
+import { cookies } from "next/headers";
+import RefineContext from "./_refine_context";
+import "@styles/global.css";
+
+export const metadata: Metadata = {
+	title: "Phoenix",
+	description: "Console",
+};
+
+function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	const cookieStore = cookies();
+	const lang = cookieStore.get("NEXT_LOCALE");
+	return (
+		<html lang={lang?.value || "en"}>
+			<body suppressHydrationWarning>
+				<Suspense>
+					<RefineContext>{children}</RefineContext>
+				</Suspense>
+			</body>
+		</html>
+	);
+}
+
+export default RootLayout;
