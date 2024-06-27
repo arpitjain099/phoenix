9f2d1a6ea0e510e4b593a1baf8d20565f1d6e7e9
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-06-27 14:29:55 +0200

    Phiphi: add error check routes

--- python/projects/phiphi/phiphi/api/health_check.py
+++ python/projects/phiphi/phiphi/api/health_check.py
@@ -21,3 +21,15 @@ async def health_check() -> HealthCheck:
         title=config.settings.TITLE,
         version=config.settings.VERSION,
     )
+
+
+@router.get("/errors/internal", tags=["status"])
+async def errors_internal() -> None:
+    """Raise an internal server error."""
+    _ = 1 / 0  # noqa: F841
+
+
+@router.get("/errors/500", tags=["status"])
+async def errors_500() -> None:
+    """Raise a 500 error."""
+    raise Exception("This is a test exception")
