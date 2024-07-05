954a82d6e244c3eab6807a1d1d9733a9d1aa954d
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-07-05 16:20:08 +0200

    Phiphi: rename environment workspace phiphi/api/workspaces/crud.py

--- python/projects/phiphi/phiphi/api/workspaces/crud.py
+++ python/projects/phiphi/phiphi/api/workspaces/crud.py
@@ -1,98 +1,84 @@
-"""Environment crud functionality."""
+"""Workspace crud functionality."""
 import slugify
 import sqlalchemy.orm
 from phiphi.api import utility
-from phiphi.api.environments import models, schemas
+from phiphi.api.workspaces import models, schemas
 
 
-def create_environment(
-    session: sqlalchemy.orm.Session, environment: schemas.EnvironmentCreate
-) -> schemas.EnvironmentResponse:
-    """Create a new environment."""
+def create_workspace(
+    session: sqlalchemy.orm.Session, workspace: schemas.WorkspaceCreate
+) -> schemas.WorkspaceResponse:
+    """Create a new workspace."""
     slug_exist = (
-        session.query(models.Environment)
-        .filter(models.Environment.slug == environment.slug)
-        .first()
+        session.query(models.Workspace).filter(models.Workspace.slug == workspace.slug).first()
     )
 
     if slug_exist:
         raise Exception("Slug already exists")
 
-    environment.name = slugify.slugify(environment.name)
-    db_environment = models.Environment(**environment.dict())
-    session.add(db_environment)
+    workspace.name = slugify.slugify(workspace.name)
+    db_workspace = models.Workspace(**workspace.dict())
+    session.add(db_workspace)
     session.commit()
-    session.refresh(db_environment)
-    return schemas.EnvironmentResponse.model_validate(db_environment)
+    session.refresh(db_workspace)
+    return schemas.WorkspaceResponse.model_validate(db_workspace)
 
 
-def get_environment(
-    session: sqlalchemy.orm.Session, slug: str
-) -> schemas.EnvironmentResponse | None:
-    """Get an environment."""
-    db_environment = (
-        session.query(models.Environment).filter(models.Environment.slug == slug).first()
-    )
+def get_workspace(session: sqlalchemy.orm.Session, slug: str) -> schemas.WorkspaceResponse | None:
+    """Get an workspace."""
+    db_workspace = session.query(models.Workspace).filter(models.Workspace.slug == slug).first()
 
-    if db_environment is None:
+    if db_workspace is None:
         return None
-    return schemas.EnvironmentResponse.model_validate(db_environment)
+    return schemas.WorkspaceResponse.model_validate(db_workspace)
 
 
-def get_environment_by_slug(
+def get_workspace_by_slug(
     session: sqlalchemy.orm.Session, slug: str
-) -> schemas.EnvironmentResponse | None:
-    """Get an environment."""
-    db_environment = (
-        session.query(models.Environment).filter(models.Environment.slug == slug).first()
-    )
-    if db_environment is None:
+) -> schemas.WorkspaceResponse | None:
+    """Get an workspace."""
+    db_workspace = session.query(models.Workspace).filter(models.Workspace.slug == slug).first()
+    if db_workspace is None:
         return None
-    return schemas.EnvironmentResponse.model_validate(db_environment)
+    return schemas.WorkspaceResponse.model_validate(db_workspace)
 
 
-def get_environments(
+def get_workspaces(
     session: sqlalchemy.orm.Session, start: int = 0, end: int = 100
-) -> list[schemas.EnvironmentResponse]:
-    """Get environments."""
-    query = sqlalchemy.select(models.Environment).offset(start).limit(end)
-    environments = session.scalars(query).all()
-    if not environments:
+) -> list[schemas.WorkspaceResponse]:
+    """Get workspaces."""
+    query = sqlalchemy.select(models.Workspace).offset(start).limit(end)
+    workspaces = session.scalars(query).all()
+    if not workspaces:
         return []
-    return [
-        schemas.EnvironmentResponse.model_validate(environment) for environment in environments
-    ]
+    return [schemas.WorkspaceResponse.model_validate(workspace) for workspace in workspaces]
 
 
-def update_environment(
-    session: sqlalchemy.orm.Session, environment_id: int, environment: schemas.EnvironmentUpdate
-) -> schemas.EnvironmentResponse | None:
-    """Update an environment."""
-    db_environment = session.get(models.Environment, environment_id)
-    if db_environment is None:
+def update_workspace(
+    session: sqlalchemy.orm.Session, workspace_id: int, workspace: schemas.WorkspaceUpdate
+) -> schemas.WorkspaceResponse | None:
+    """Update an workspace."""
+    db_workspace = session.get(models.Workspace, workspace_id)
+    if db_workspace is None:
         return None
-    for field, value in environment.dict(exclude_unset=True).items():
-        setattr(db_environment, field, value)
+    for field, value in workspace.dict(exclude_unset=True).items():
+        setattr(db_workspace, field, value)
     session.commit()
-    session.refresh(db_environment)
-    return schemas.EnvironmentResponse.model_validate(db_environment)
+    session.refresh(db_workspace)
+    return schemas.WorkspaceResponse.model_validate(db_workspace)
 
 
-def get_unique_slug(
-    session: sqlalchemy.orm.Session, environment_name: str
-) -> schemas.SlugResponse:
+def get_unique_slug(session: sqlalchemy.orm.Session, workspace_name: str) -> schemas.SlugResponse:
     """Get unique slug."""
     # validation for empty string
-    if not environment_name.strip():
+    if not workspace_name.strip():
         raise Exception("Please enter a valid string")
 
     slug_exist = (
-        session.query(models.Environment)
-        .filter(models.Environment.slug == environment_name)
-        .first()
+        session.query(models.Workspace).filter(models.Workspace.slug == workspace_name).first()
     )
 
-    name = slugify.slugify(environment_name)
+    name = slugify.slugify(workspace_name)
 
     if slug_exist:
         random_str = utility.generate_random_string(4)
