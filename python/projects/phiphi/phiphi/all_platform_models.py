0fac130bc33f62f1780c31755e0c8b1ea0919d7a
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-07-05 16:06:49 +0200

    Phiphi: rename environment workspace phiphi/all_platform_models.py

--- python/projects/phiphi/phiphi/all_platform_models.py
+++ python/projects/phiphi/phiphi/all_platform_models.py
@@ -4,7 +4,7 @@ This module is used to import all models of the application to be used in the
 Alembic migrations and testing.
 """
 from phiphi import platform_db
-from phiphi.api.environments import models as environment_models  # noqa: F401
+from phiphi.api.workspaces import models as workspace_models  # noqa: F401
 from phiphi.api.projects import models as project_models  # noqa: F401
 from phiphi.api.projects.gathers import models as gather_models  # noqa: F401
 from phiphi.api.projects.gathers.apify_facebook_comments import (
