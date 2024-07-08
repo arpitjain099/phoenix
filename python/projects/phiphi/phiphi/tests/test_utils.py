2ea6b4e7f1453e0b60f72473761f30c9801f7503
dbcerigo <dan@datavaluepeople.com>
2024-07-08 14:33:42 +0100

    Phiphi: "env"->"workspace" in test_utils

--- python/projects/phiphi/phiphi/tests/test_utils.py
+++ python/projects/phiphi/phiphi/tests/test_utils.py
@@ -5,39 +5,37 @@ from phiphi import utils
 
 MAIN_APIFY_API_KEY = "main_apify_key"
 OTHER_APIFY_API_KEY = "other_apify_key"
-OTHER_ENV_SLUG = "other_env_slug"
-APIFY_API_KEYS_STR_1 = (
-    f'{{"main_env_slug": "{MAIN_APIFY_API_KEY}", "{OTHER_ENV_SLUG}": "{OTHER_APIFY_API_KEY}"}}'
-)
-SETTINGS_ENV_MAIN = {
+OTHER_WORKSPACE_SLUG = "other_workspace_slug"
+APIFY_API_KEYS_STR_1 = f'{{"main_workspace_slug": "{MAIN_APIFY_API_KEY}", "{OTHER_WORKSPACE_SLUG}": "{OTHER_APIFY_API_KEY}"}}'  # noqa: E501
+SETTINGS_WORKSPACE_MAIN = {
     "APIFY_API_KEYS": APIFY_API_KEYS_STR_1,
-    "FIRST_WORKSPACE_SLUG": "main_env_slug",
+    "FIRST_WORKSPACE_SLUG": "main_workspace_slug",
 }
-SETTINGS_ENV_OTHER = {
+SETTINGS_WORKSPACE_OTHER = {
     "APIFY_API_KEYS": APIFY_API_KEYS_STR_1,
-    "FIRST_WORKSPACE_SLUG": OTHER_ENV_SLUG,
+    "FIRST_WORKSPACE_SLUG": OTHER_WORKSPACE_SLUG,
 }
 
 
-@pytest.mark.patch_settings(SETTINGS_ENV_MAIN)
+@pytest.mark.patch_settings(SETTINGS_WORKSPACE_MAIN)
 def test_get_apify_keys_main(patch_settings):
     """Test get apify keys."""
     assert utils.get_apify_api_key() == MAIN_APIFY_API_KEY
 
 
-@pytest.mark.patch_settings(SETTINGS_ENV_MAIN)
+@pytest.mark.patch_settings(SETTINGS_WORKSPACE_MAIN)
 def test_get_apify_keys_other(patch_settings):
-    """Test get apify keys for other env."""
-    assert utils.get_apify_api_key(OTHER_ENV_SLUG) == OTHER_APIFY_API_KEY
+    """Test get apify keys for other workspace."""
+    assert utils.get_apify_api_key(OTHER_WORKSPACE_SLUG) == OTHER_APIFY_API_KEY
 
 
-@pytest.mark.patch_settings(SETTINGS_ENV_OTHER)
-def test_get_apify_keys_other_env(patch_settings):
+@pytest.mark.patch_settings(SETTINGS_WORKSPACE_OTHER)
+def test_get_apify_keys_other_workspace(patch_settings):
     """Test get apify keys."""
     assert utils.get_apify_api_key() == OTHER_APIFY_API_KEY
 
 
-@pytest.mark.patch_settings(SETTINGS_ENV_MAIN)
+@pytest.mark.patch_settings(SETTINGS_WORKSPACE_MAIN)
 def test_get_apify_keys_not_found(patch_settings):
     """Test get apify keys."""
     with pytest.raises(ValueError):
@@ -47,7 +45,7 @@ def test_get_apify_keys_not_found(patch_settings):
 @pytest.mark.patch_settings(
     {"APIFY_API_KEYS": APIFY_API_KEYS_STR_1, "FIRST_WORKSPACE_SLUG": "not_found"}
 )
-def test_get_apify_keys_not_found_env(patch_settings):
+def test_get_apify_keys_not_found_workspace(patch_settings):
     """Test get apify keys."""
     with pytest.raises(ValueError):
         utils.get_apify_api_key()
