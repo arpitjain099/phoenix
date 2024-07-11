154d03531db3e107c9cba441e4f712f342cf1a7d
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-07-11 13:51:09 +0200

    Phiphi: add gather.constants.TIKTOK_POST_SEARCH_SECTION
    This contant will be be used across the tiktok post gathers.

--- python/projects/phiphi/phiphi/api/projects/gathers/__init__.py
+++ python/projects/phiphi/phiphi/api/projects/gathers/__init__.py
@@ -4,6 +4,7 @@ from phiphi.api.projects.gathers import (
     apify_facebook_posts,  # noqa: F401
     child_crud,  # noqa: F401
     child_types,  # noqa: F401
+    constants,  # noqa: F401
     crud,  # noqa: F401
     schemas,  # noqa: F401
 )
