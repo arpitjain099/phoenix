da00180b62fc4a400a9fa778a50b32c40b589049
dbcerigo <dan@datavaluepeople.com>
2024-07-17 15:02:49 +0100

    API: rename `db` to `orm` in classifiers crud

--- python/projects/phiphi/phiphi/api/projects/classifiers/crud.py
+++ python/projects/phiphi/phiphi/api/projects/classifiers/crud.py
@@ -10,26 +10,26 @@ def create_classifier(
     session: sqlalchemy.orm.Session, classifier: schemas.ClassifierCreate
 ) -> schemas.ClassifierResponse:
     """Create a new classifier."""
-    db_classifier = models.Classifiers(
+    orm_classifier = models.Classifiers(
         **classifier.dict(), created_at=datetime.now(), archived_at=None
     )
-    session.add(db_classifier)
+    session.add(orm_classifier)
     session.commit()
-    session.refresh(db_classifier)
-    return schemas.ClassifierResponse.model_validate(db_classifier)
+    session.refresh(orm_classifier)
+    return schemas.ClassifierResponse.model_validate(orm_classifier)
 
 
 def get_classifier(
     session: sqlalchemy.orm.Session, classifier_id: int
 ) -> schemas.ClassifierResponse | None:
     """Get a classifier."""
-    db_classifier = (
+    orm_classifier = (
         session.query(models.Classifiers).filter(models.Classifiers.id == classifier_id).first()
     )
 
-    if db_classifier is None:
+    if orm_classifier is None:
         return None
-    return schemas.ClassifierResponse.model_validate(db_classifier)
+    return schemas.ClassifierResponse.model_validate(orm_classifier)
 
 
 def get_classifiers(
@@ -41,10 +41,10 @@ def get_classifiers(
     query = session.query(models.Classifiers).filter(models.Classifiers.project_id == project_id)
     if not included_deleted:
         query = query.filter(models.Classifiers.archived_at.is_(None))
-    db_classifiers = query.order_by(models.Classifiers.id.desc()).all()
+    orm_classifiers = query.order_by(models.Classifiers.id.desc()).all()
     return [
-        schemas.ClassifierResponse.model_validate(db_classifier)
-        for db_classifier in db_classifiers
+        schemas.ClassifierResponse.model_validate(orm_classifier)
+        for orm_classifier in orm_classifiers
     ]
 
 
@@ -55,33 +55,33 @@ def archive_classifier(
 
     Sets the archived_at col to current datetime.
     """
-    db_classifier = (
+    orm_classifier = (
         session.query(models.Classifiers).filter(models.Classifiers.id == classifier_id).first()
     )
-    if db_classifier:
-        db_classifier.archived_at = datetime.now()
+    if orm_classifier:
+        orm_classifier.archived_at = datetime.now()
         session.commit()
-        session.refresh(db_classifier)
-    return schemas.ClassifierResponse.model_validate(db_classifier)
+        session.refresh(orm_classifier)
+    return schemas.ClassifierResponse.model_validate(orm_classifier)
 
 
 def create_class(
     session: sqlalchemy.orm.Session, class_: schemas.ClassCreate
 ) -> schemas.ClassResponse:
     """Create a new class."""
-    db_class = models.Classes(**class_.dict(), last_updated_at=datetime.now())
-    session.add(db_class)
+    orm_class = models.Classes(**class_.dict(), last_updated_at=datetime.now())
+    session.add(orm_class)
     session.commit()
-    session.refresh(db_class)
-    return schemas.ClassResponse.model_validate(db_class)
+    session.refresh(orm_class)
+    return schemas.ClassResponse.model_validate(orm_class)
 
 
 def get_class(session: sqlalchemy.orm.Session, class_id: int) -> schemas.ClassResponse | None:
     """Get a class."""
-    db_class = session.query(models.Classes).filter(models.Classes.id == class_id).first()
-    if db_class is None:
+    orm_class = session.query(models.Classes).filter(models.Classes.id == class_id).first()
+    if orm_class is None:
         return None
-    return schemas.ClassResponse.model_validate(db_class)
+    return schemas.ClassResponse.model_validate(orm_class)
 
 
 def get_classes(
@@ -89,17 +89,17 @@ def get_classes(
     project_id: int,
 ) -> list[schemas.ClassResponse]:
     """Get classes."""
-    db_classes = (
+    orm_classes = (
         session.query(models.Classes).filter(models.Classes.project_id == project_id).all()
     )
-    return [schemas.ClassResponse.model_validate(db_class) for db_class in db_classes]
+    return [schemas.ClassResponse.model_validate(orm_class) for orm_class in orm_classes]
 
 
 def delete_class(session: sqlalchemy.orm.Session, class_id: int) -> None:
     """Delete a class."""
-    db_class = session.query(models.Classes).filter(models.Classes.id == class_id).first()
-    if db_class:
-        session.delete(db_class)
+    orm_class = session.query(models.Classes).filter(models.Classes.id == class_id).first()
+    if orm_class:
+        session.delete(orm_class)
         session.commit()
 
 
@@ -107,11 +107,11 @@ def update_class(
     session: sqlalchemy.orm.Session, class_id: int, class_: schemas.ClassUpdate
 ) -> schemas.ClassResponse | None:
     """Update a class."""
-    db_class = session.get(models.Classes, class_id)
-    if db_class:
+    orm_class = session.get(models.Classes, class_id)
+    if orm_class:
         for field, value in class_.dict(exclude={"id"}).items():
-            setattr(db_class, field, value)
-        db_class.last_updated_at = datetime.now()
+            setattr(orm_class, field, value)
+        orm_class.last_updated_at = datetime.now()
         session.commit()
-        session.refresh(db_class)
-    return schemas.ClassResponse.model_validate(db_class)
+        session.refresh(orm_class)
+    return schemas.ClassResponse.model_validate(orm_class)
