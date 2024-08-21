9b58dea2870d805a43c9d6df8c607f1860aaff23
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-08-21 11:55:45 +0200

    Phiphi: add test_normaliser_tiktok_hashtags_posts

--- python/projects/phiphi/phiphi/tests/pipeline_jobs/gathers/test_normalisers.py
+++ python/projects/phiphi/phiphi/tests/pipeline_jobs/gathers/test_normalisers.py
@@ -77,3 +77,28 @@ def test_normaliser_tiktok_accounts_posts(
     )
 
     pd.testing.assert_frame_equal(processed_df, normalised_tiktok_accounts_posts_df)
+
+
+@pytest.mark.freeze_time("2024-04-02T12:10:59.000Z")
+def test_normaliser_tiktok_hashtags_posts(
+    normalised_tiktok_hashtags_posts_df, tiktok_hashtags_posts_gather_fixture
+):
+    """Test normaliser for tiktok hashtags posts function.
+
+    Note: we use the `normalise_batch` function from the `normalise` module to test the normaliser,
+    as this is an easy way to test multiple records (and tests in the usage context).
+    """
+    batch_json = utils.load_sample_raw_data(
+        child_type_name=schemas.ChildTypeName.apify_tiktok_hashtags_posts,
+    )
+
+    processed_df = normalise.normalise_batch(
+        # all tiktok posts gathers are normalised the same way
+        normaliser=normalisers.normalise_single_tiktok_posts_json,
+        batch_json=batch_json,
+        gather=tiktok_hashtags_posts_gather_fixture,
+        gather_batch_id=3,
+        gathered_at=datetime.fromisoformat("2024-04-01T12:00:00.000Z"),
+    )
+
+    pd.testing.assert_frame_equal(processed_df, normalised_tiktok_hashtags_posts_df)
