a92409f0ecc0978a0f3a807dd4a0f343df94ba13
andrewsutjahjo <andrewsutjahjo@gmail.com>
2024-08-23 14:17:22 +0700

    Phiphi: Add Documentation for gather flow

--- python/projects/phiphi/phiphi/pipeline_jobs/gathers/flow.py
+++ python/projects/phiphi/phiphi/pipeline_jobs/gathers/flow.py
@@ -21,6 +21,8 @@ def gather_flow(
     Warning: there is a race condition in this flow for the deduplicate step if multiple gathers
     flow are being run at the same time. Very unlikely though.
     """
+    # Create the gather object from the gather_dict as prefect can't parse it automatically from
+    # the parameters
     gather = gathers.child_types.get_response_type(gather_child_type)(**gather_dict)
 
     scrape_response = apify_scrape.apify_scrape_and_batch_download_results(
