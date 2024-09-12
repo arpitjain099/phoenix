6008a2f4935fc552e5c5511b9dec608d9b2e96d8
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-09-12 16:04:18 +0200

    Phiphi: fix the multiheads alembic

--- python/projects/phiphi/phiphi/migrations/versions/platform/r_2024_09_06_1058_323e15610133_remove_mistaken_columns_apify_tiktok_.py
+++ python/projects/phiphi/phiphi/migrations/versions/platform/r_2024_09_06_1058_323e15610133_remove_mistaken_columns_apify_tiktok_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "323e15610133"
-down_revision: Union[str, None] = "d31116c6bea5"
+down_revision: Union[str, None] = "b94b5e2a7757"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
