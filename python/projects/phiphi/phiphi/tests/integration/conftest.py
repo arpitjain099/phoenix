55480967b7760fbad037fdd4a451c8b352c99626
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-09-19 13:56:31 +0200

    Phiphi: rename temp_project_namespace -> tmp_project_namespace

--- python/projects/phiphi/phiphi/tests/integration/conftest.py
+++ python/projects/phiphi/phiphi/tests/integration/conftest.py
@@ -7,19 +7,19 @@ from phiphi.pipeline_jobs import projects
 
 
 @pytest.fixture
-def temp_project_namespace():
+def tmp_project_namespace():
     """Generate a temporary project namespace for testing."""
-    temp_project_namespace = str(uuid.uuid4())[:10]
-    temp_project_namespace = temp_project_namespace.replace("-", "")
-    return f"test_{temp_project_namespace}"
+    tmp_project_namespace_hash = str(uuid.uuid4())[:10]
+    tmp_project_namespace_hash = tmp_project_namespace_hash.replace("-", "")
+    return f"test_{tmp_project_namespace_hash}"
 
 
 @pytest.fixture
-def tmp_bq_project(temp_project_namespace):
+def tmp_bq_project(tmp_project_namespace):
     """Setup and breakdown of a test project namespace wrapper for integration tests."""
-    print(f"Test project namespace: {temp_project_namespace}")
-    projects.init_project_db.fn(temp_project_namespace, with_dummy_data=True)
+    print(f"Test project namespace: {tmp_project_namespace}")
+    projects.init_project_db.fn(tmp_project_namespace, with_dummy_data=True)
 
-    yield temp_project_namespace
+    yield tmp_project_namespace
 
-    projects.delete_project_db.fn(temp_project_namespace)
+    projects.delete_project_db.fn(tmp_project_namespace)
--- python/projects/phiphi/phiphi/tests/integration/test_bq_project_init.py
+++ python/projects/phiphi/phiphi/tests/integration/test_bq_project_init.py
@@ -5,22 +5,22 @@ from google.cloud import bigquery
 from phiphi.pipeline_jobs import constants, projects
 
 
-def test_bq_project_init(temp_project_namespace):
+def test_bq_project_init(tmp_project_namespace):
     """Test project init function with a real Bigquery project.
 
     If the test fails you may need to manually clean up (delete) the dataset within Bigquery.
     """
-    projects.init_project_db.fn(temp_project_namespace, with_dummy_data=True)
+    projects.init_project_db.fn(tmp_project_namespace, with_dummy_data=True)
 
     client = bigquery.Client()
-    assert client.get_dataset(temp_project_namespace)
+    assert client.get_dataset(tmp_project_namespace)
     # Check that the dummy tabulated messages has been created
-    assert client.get_table(f"{temp_project_namespace}.{constants.TABULATED_MESSAGES_TABLE_NAME}")
+    assert client.get_table(f"{tmp_project_namespace}.{constants.TABULATED_MESSAGES_TABLE_NAME}")
 
     # Check that will not fail if the dataset already exists.
-    dataset = projects.init_project_db.fn(temp_project_namespace)
+    dataset = projects.init_project_db.fn(tmp_project_namespace)
     assert client.get_dataset(dataset)
 
-    projects.delete_project_db.fn(temp_project_namespace)
+    projects.delete_project_db.fn(tmp_project_namespace)
     with pytest.raises(Exception):
         client.get_dataset(dataset)
