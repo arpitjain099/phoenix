4bf48f5855e45e2d1a177bd84c2a4dd1c0431c8d
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-09-19 14:24:13 +0200

    Phiphi: remove unneeded classified_messages schemas

--- python/projects/phiphi/phiphi/pipeline_jobs/classify/bq_table_schema.py
+++
@@ -1,8 +0,0 @@
-"""Utils for classified_messages BigQuery table schema."""
-import pathlib
-
-
-def get_path() -> str:
-    """Get the schema path."""
-    module_dir = pathlib.Path(__file__).resolve().parent
-    return str(module_dir / "classified_messages.schema.json")
