1f46e3c36a8276c9ddb80dbf564830af16a5df27
dbcerigo <dan@datavaluepeople.com>
2024-10-01 22:38:01 +0100

    Phiphi: fix bq migration
    BQ doesn't allow adding columns that have `Nullable=False` (as when you
    added it it has to have Nulls). So instead of creating a new migration,
    I edited the old one. Note though that this could mean we can't use
    `Nullable=False` columns in future migrations.

--- python/projects/phiphi/phiphi/project_db_migrations/versions/project/r_2024_09_18_1336_82d406d86348_add_classified_messages.py
+++ python/projects/phiphi/phiphi/project_db_migrations/versions/project/r_2024_09_18_1336_82d406d86348_add_classified_messages.py
@@ -22,7 +22,8 @@ def upgrade() -> None:
     op.create_table(
         "classified_messages",
         sa.Column("classifier_id", sa.Integer(), nullable=False),
-        sa.Column("class_id", sa.Integer(), nullable=False),
+        sa.Column("classifier_version_id", sa.Integer(), nullable=False),
+        sa.Column("class_name", sa.String(), nullable=False),
         sa.Column("phoenix_platform_message_id", sa.String(), nullable=False),
         sa.Column("job_run_id", sa.Integer(), nullable=False),
     )
