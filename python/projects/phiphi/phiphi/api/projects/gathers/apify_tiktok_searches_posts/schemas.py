6ca597ec59e7f3392ef3ac31eec46e56f52334ef
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-10-01 15:30:54 +0200

    Phiphi: add Create Update schemas to apify_tiktok_searches_posts

--- python/projects/phiphi/phiphi/api/projects/gathers/apify_tiktok_searches_posts/schemas.py
+++ python/projects/phiphi/phiphi/api/projects/gathers/apify_tiktok_searches_posts/schemas.py
@@ -50,3 +50,38 @@ class ApifyTikTokSearchesPostsGatherResponse(
         # We are adding searchSection to the dictionary as it is a constant for gathering posts.
         apify_dict["searchSection"] = constants.TIKTOK_POST_SEARCH_SECTION
         return apify_dict
+
+
+class ApifyTikTokSearchesPostsGatherCreate(
+    gather_schemas.GatherCreate, ApifyTikTokSearchesPostsGatherBase
+):
+    """Apify Gather create schema.
+
+    Properties to receive via API on creation.
+    """
+
+
+class ApifyTikTokSearchesPostsGatherUpdate(gather_schemas.GatherUpdate):
+    """Apify Gather update schema."""
+
+    limit_posts_per_search: Optional[int] = pydantic.Field(
+        serialization_alias="resultsPerPage", description="Limit results per search"
+    )
+    # It is important that the name of the property is different from the alias other wise it will
+    # not be returned from serialize_to_apify_input
+    search_list: Optional[list[str]] = pydantic.Field(
+        serialization_alias="searches",
+        description=(
+            "List of searches to scrape TikTok videos for. "
+            " It is recommended to use without # prefix but "
+            "there seems to be no difference when using with."
+        ),
+    )
+    proxy_country_to_gather_from: Optional[str] = pydantic.Field(
+        default=None,
+        serialization_alias="proxyCountryCode",
+        description=(
+            "Country to use for the proxy to gather from. "
+            "If this is set a RESIDENTIAL group will be used and will increase the price."
+        ),
+    )
