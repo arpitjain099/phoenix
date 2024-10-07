955ce374fb5ead23c6ed32540041c042a069184b
dbcerigo <dan@datavaluepeople.com>
2024-10-07 16:45:06 +0100

    Phiphi: fix classifier model typecheck errors

--- python/projects/phiphi/phiphi/api/projects/classifiers/models.py
+++ python/projects/phiphi/phiphi/api/projects/classifiers/models.py
@@ -9,6 +9,25 @@ from phiphi import platform_db
 from phiphi.api import base_models
 
 
+class ClassifierVersionsBase(platform_db.Base):
+    """Classifier versions table."""
+
+    __abstract__ = True
+
+    __tablename__ = "classifier_versions"
+
+    version_id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    classifier_id: orm.Mapped[int] = orm.mapped_column(sa.ForeignKey("classifiers.id"))
+    classes_dict: orm.Mapped[Dict[str, str]] = orm.mapped_column(base_models.JSONEncodedValue)
+    params: orm.Mapped[Optional[str]] = orm.mapped_column(base_models.JSONEncodedValue)
+
+
+class ClassifierVersions(ClassifierVersionsBase, base_models.TimestampModel):
+    """Classifier versions table."""
+
+    __tablename__ = "classifier_versions"
+
+
 class ClassifiersBase(platform_db.Base):
     """Classifiers model."""
 
@@ -27,7 +46,8 @@ class Classifiers(ClassifiersBase, base_models.TimestampModel):
     __tablename__ = "classifiers"
 
     # Relationship to ClassifierVersions
-    classifier_versions: orm.Mapped[list["ClassifierVersions"]] = orm.relationship(
+    classifier_versions = orm.relationship(
+        "ClassifierVersions",
         back_populates="classifier",
         cascade="all, delete-orphan",
         order_by="ClassifierVersions.version_id.desc()",  # Ensures ordered by desc version_id
@@ -35,29 +55,12 @@ class Classifiers(ClassifiersBase, base_models.TimestampModel):
     )
 
     @property
-    def latest_version(self) -> "ClassifierVersions":
+    def latest_version(self) -> ClassifierVersions:
         """Get the latest version of the classifier."""
-        return self.classifier_versions.first()
+        latest_version: ClassifierVersions = self.classifier_versions.first()
+        return latest_version
 
-    def get_all_versions(self) -> list["ClassifierVersions"]:
+    def all_versions(self) -> list[ClassifierVersions]:
         """Get all versions of the classifier."""
-        return self.classifier_versions.all()
-
-
-class ClassifierVersionsBase(platform_db.Base):
-    """Classifier versions table."""
-
-    __abstract__ = True
-
-    __tablename__ = "classifier_versions"
-
-    version_id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
-    classifier_id: orm.Mapped[int] = orm.mapped_column(sa.ForeignKey("classifiers.id"))
-    classes_dict: orm.Mapped[Dict[str, str]] = orm.mapped_column(base_models.JSONEncodedValue)
-    params: orm.Mapped[Optional[str]] = orm.mapped_column(base_models.JSONEncodedValue)
-
-
-class ClassifierVersions(ClassifierVersionsBase, base_models.TimestampModel):
-    """Classifier versions table."""
-
-    __tablename__ = "classifier_versions"
+        latest_versions: list[ClassifierVersions] = self.classifier_versions.all()
+        return latest_versions
