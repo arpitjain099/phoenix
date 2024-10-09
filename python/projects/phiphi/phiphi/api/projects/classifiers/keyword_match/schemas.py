58f5b46e0c8e322bf2d694d22f316f94e7861a85
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-10-09 09:08:14 +0200

    PhiPhi: add keyword_match/schemas.py
    This will replace the keyword_match schemas currently in
    classifiers/schemas.py at some point.

---
+++ python/projects/phiphi/phiphi/api/projects/classifiers/keyword_match/schemas.py
@@ -0,0 +1,48 @@
+"""Keyword Match Schemas."""
+from typing import Annotated
+
+import pydantic
+from typing_extensions import TypedDict
+
+from phiphi.api.projects.classifiers import base_schemas
+
+
+class ClassToKeywordConfig(TypedDict):
+    """Class to keyword config containing keyword matches."""
+
+    class_name: str
+    musts: Annotated[
+        str,
+        pydantic.Field(
+            description="Whitespace separated list of keywords that must all match to classify "
+            "as this class"
+        ),
+    ]
+    # These are optional upgrades which are not going to be implemented in the first version
+    # shoulds: Annotated[str, pydantic.Field(description="Whitespace separated list of keywords
+    # of which one must match to classify as this class")]
+    # nots: Annotated[str, pydantic.Field(description="Whitespace separated list of keywords
+    #     of which none can match to classify as this class")]
+
+
+class KeywordMatchParams(TypedDict):
+    """Params subschema for keyword match classifier."""
+
+    class_to_keyword_configs: list[ClassToKeywordConfig]
+
+
+class KeywordMatchVersionBase(pydantic.BaseModel):
+    """Keyword match version base schema."""
+
+    classes_dict: base_schemas.ClassesDictType
+    params: KeywordMatchParams
+
+
+class KeywordMatchVersionCreate(KeywordMatchVersionBase, base_schemas.ClassifierVersionCreate):
+    """Keyword match version create schema."""
+
+
+class KeywordMatchClassifierCreate(base_schemas.ClassifierCreate):
+    """Keyword match classifier create."""
+
+    version: KeywordMatchVersionCreate
