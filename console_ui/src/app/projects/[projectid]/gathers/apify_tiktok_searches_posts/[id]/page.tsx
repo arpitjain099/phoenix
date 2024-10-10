a108df10f55f6a23adf4fc342670851a0fbd7574
Chisom Marshall Ogbuagu <36017222+chiysom@users.noreply.github.com>
2024-10-10 11:50:53 +0100

    Console: tiktok searches post

---
+++ console_ui/src/app/projects/[projectid]/gathers/apify_tiktok_searches_posts/[id]/page.tsx
@@ -0,0 +1,166 @@
+"use client";
+
+import React from "react";
+import { useShow, useTranslate } from "@refinedev/core";
+import { Show, TextField, NumberField } from "@refinedev/mantine";
+import { Accordion, Container, Group, Title } from "@mantine/core";
+import { useParams } from "next/navigation";
+import URLInputList from "@components/gather/url-list";
+import GatherViewStatus from "@components/gather/view_status";
+import { searchPostBaseLink } from "src/utils/constants";
+import GatherViewBreadcrumb from "@components/breadcrumbs/gatherView";
+import GatherViewHeaderButton from "@components/gather/header-button";
+
+export default function ApifyTiktokSearchPostShow(): JSX.Element {
+	const { projectid, id } = useParams();
+	const translate = useTranslate();
+	const { queryResult } = useShow({
+		resource: `projects/${projectid}/gathers`,
+		id: id as string,
+	});
+
+	const { data, isLoading } = queryResult;
+
+	const record = data?.data;
+
+	return (
+		<Show
+			title={<Title order={3}>{record?.name}</Title>}
+			breadcrumb={
+				<GatherViewBreadcrumb record={record} projectid={projectid as string} />
+			}
+			isLoading={isLoading}
+			headerButtons={() => (
+				<GatherViewHeaderButton
+					resource="apify_tiktok_searches_posts"
+					id={id as string}
+					projectid={projectid as string}
+					record={record}
+					isLoading={isLoading}
+				/>
+			)}
+		>
+			<TextField
+				value={translate("gathers.types.apify_tiktok_searches_posts.view.text")}
+			/>
+			<div className="w-full">
+				<Accordion
+					styles={{
+						control: {
+							paddingLeft: 0,
+						},
+						item: {
+							"&[data-active]": {
+								backgroundColor: "none",
+							},
+						},
+					}}
+					defaultValue="status"
+				>
+					<Accordion.Item value="status" className="mb-4">
+						<Accordion.Control>
+							<Title order={5}>
+								{translate(
+									"gathers.types.apify_tiktok_searches_posts.view.accordion.status"
+								)}
+							</Title>
+						</Accordion.Control>
+						<Accordion.Panel>
+							<GatherViewStatus record={record} />
+						</Accordion.Panel>
+					</Accordion.Item>
+					<Accordion.Item value="general" mb="md">
+						<Accordion.Control>
+							<Title order={5}>
+								{translate(
+									"gathers.types.apify_tiktok_searches_posts.view.accordion.general"
+								)}
+							</Title>
+						</Accordion.Control>
+						<Accordion.Panel>
+							<Container className="mx-0 flex flex-col my-4">
+								<Group>
+									<Title my="xs" order={5}>
+										{translate("gathers.fields.from")}:
+									</Title>
+									<TextField
+										className="capitalize"
+										value={translate(`gathers.fields.source.apify`)}
+									/>
+								</Group>
+								<Group>
+									<Title my="xs" order={5}>
+										{translate("gathers.fields.platform")}:
+									</Title>
+									<TextField
+										className="capitalize"
+										value={translate("gathers.fields.source.tiktok")}
+									/>
+								</Group>
+								<Group>
+									<Title my="xs" order={5}>
+										{translate("gathers.fields.data_type")}:
+									</Title>
+									<TextField
+										className="capitalize"
+										value={translate("gathers.fields.source.search_posts")}
+									/>
+								</Group>
+								<Group>
+									<Title my="xs" order={5}>
+										{translate(
+											"gathers.types.apify_tiktok_searches_posts.fields.limit_posts_per_search"
+										)}
+										:
+									</Title>
+									<NumberField value={record?.limit_posts_per_search} />
+								</Group>
+								<Group>
+									<Title my="xs" order={5}>
+										{translate(
+											"gathers.types.apify_tiktok_searches_posts.fields.proxy_country_to_gather_from"
+										)}
+										:
+									</Title>
+									<TextField
+										className="capitalize"
+										value={record?.proxy_country_to_gather_from}
+									/>
+								</Group>
+							</Container>
+						</Accordion.Panel>
+					</Accordion.Item>
+					<Accordion.Item value="source" mb="md">
+						<Accordion.Control>
+							<Title order={5}>
+								{translate(
+									`gathers.types.apify_tiktok_searches_posts.view.accordion.source`
+								)}
+							</Title>
+						</Accordion.Control>
+						<Accordion.Panel>
+							<Container className="mx-0 flex flex-col my-4">
+								<Group>
+									<Title my="xs" order={5}>
+										{translate(
+											`gathers.types.apify_tiktok_searches_posts.view.accordion.source_title`
+										)}
+										:
+									</Title>
+									{record?.search_list?.length}{" "}
+									{translate(`gathers.fields.source.input_values`)}
+								</Group>
+								{record?.search_list?.length > 0 && (
+									<URLInputList
+										list={record?.search_list}
+										template_url_for_input={searchPostBaseLink}
+									/>
+								)}
+							</Container>
+						</Accordion.Panel>
+					</Accordion.Item>
+				</Accordion>
+			</div>
+		</Show>
+	);
+}
