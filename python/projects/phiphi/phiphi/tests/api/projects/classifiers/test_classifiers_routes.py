ceb9bd841ccf123d955d6c285991fa7af9df2813
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-10-13 11:36:07 +0200

    PhiPhi: add keyword_match_seed with version

--- python/projects/phiphi/phiphi/tests/api/projects/classifiers/test_classifiers_routes.py
+++ python/projects/phiphi/phiphi/tests/api/projects/classifiers/test_classifiers_routes.py
@@ -34,7 +34,7 @@ def test_get_classifiers(reseed_tables, client: TestClient) -> None:
     response = client.get("/projects/1/classifiers")
     assert response.status_code == 200
     json = response.json()
-    length = 2
+    length = 3
     assert len(json) == length
     # First classifier should be last as it is id desc
     assert json[length - 1]["id"] == keyword_match_seed.TEST_KEYWORD_CLASSIFIERS[0].id
