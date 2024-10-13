b1005d64f779c75e9f5903be5a5d89138c43161a
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-10-13 11:36:07 +0200

    PhiPhi: add keyword_match_seed with version

--- python/projects/phiphi/phiphi/seed/classifiers/keyword_match_seed.py
+++ python/projects/phiphi/phiphi/seed/classifiers/keyword_match_seed.py
@@ -2,7 +2,8 @@
 from sqlalchemy.orm import Session
 
 from phiphi.api.projects.classifiers import base_schemas, response_schemas
-from phiphi.api.projects.classifiers import crud_v2 as crud
+from phiphi.api.projects.classifiers import crud_v2 as classifier_crud
+from phiphi.api.projects.classifiers.keyword_match import crud
 
 TEST_KEYWORD_CLASSIFIER_CREATE_NO_VERSION = base_schemas.ClassifierCreate(
     name="Test keyword match Classifier 1 no version",
@@ -32,6 +33,20 @@ TEST_KEYWORD_CLASSIFIER_CREATE_ARCHIVED = base_schemas.ClassifierCreate(
     ],
 )
 
+TEST_KEYWORD_CLASSIFIER_CREATE_VERSIONED = base_schemas.ClassifierCreate(
+    name="Test keyword match Classifier 2 Versioned",
+    intermediatory_classes=[
+        base_schemas.IntermediatoryClassCreate(
+            name="Test Class 1",
+            description="Test Class 1 Description",
+        ),
+        base_schemas.IntermediatoryClassCreate(
+            name="Test Class 2",
+            description="Test Class 2 Description",
+        ),
+    ],
+)
+
 TEST_KEYWORD_CLASSIFIERS: list[response_schemas.Classifier] = []
 
 
@@ -43,7 +58,7 @@ def seed_test_classifier_keyword_match(session: Session) -> None:
     project_id = 1
 
     for classifier_create in classifiers:
-        classifier = crud.create_classifier(
+        classifier = classifier_crud.create_classifier(
             session=session,
             project_id=project_id,
             classifier_type=base_schemas.ClassifierType.keyword_match,
@@ -51,13 +66,13 @@ def seed_test_classifier_keyword_match(session: Session) -> None:
         )
         TEST_KEYWORD_CLASSIFIERS.append(classifier)
 
-    classifier = crud.create_classifier(
+    classifier = classifier_crud.create_classifier(
         session=session,
         project_id=project_id,
         classifier_type=base_schemas.ClassifierType.keyword_match,
         classifier_create=TEST_KEYWORD_CLASSIFIER_CREATE_ARCHIVED,
     )
-    archived_classifier = crud.archive_classifier(
+    archived_classifier = classifier_crud.archive_classifier(
         session=session,
         project_id=project_id,
         classifier_id=classifier.id,
@@ -65,3 +80,24 @@ def seed_test_classifier_keyword_match(session: Session) -> None:
     # Need to check for typing errors
     assert archived_classifier
     TEST_KEYWORD_CLASSIFIERS.append(archived_classifier)
+
+    classifier = classifier_crud.create_classifier(
+        session=session,
+        project_id=project_id,
+        classifier_type=base_schemas.ClassifierType.keyword_match,
+        classifier_create=TEST_KEYWORD_CLASSIFIER_CREATE_VERSIONED,
+    )
+    _ = crud.create_version(
+        session=session,
+        project_id=project_id,
+        classifier_id=classifier.id,
+    )
+    # Need to refresh the classifier so the response includes latest version
+    classifier_with_version = classifier_crud.get_classifier(
+        session=session,
+        project_id=project_id,
+        classifier_id=classifier.id,
+    )
+    # Needed for the typing
+    assert classifier_with_version
+    TEST_KEYWORD_CLASSIFIERS.append(classifier_with_version)
