095989dc8405cef154dd0b9f68c1e5a3b16ac547
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-10-17 16:00:12 +0200

    PhiPhi: implement get_pipeline_classifiers

---
+++ python/projects/phiphi/phiphi/tests/api/projects/classifiers/test_classifiers_crud.py
@@ -0,0 +1,17 @@
+"""Test classifiers crud."""
+
+from phiphi.api.projects.classifiers import crud_v2 as crud
+
+
+def test_get_pipeline_classifiers(reseed_tables):
+    """Test get pipeline classifiers."""
+    classifiers = crud.get_pipeline_classifiers(reseed_tables, 1)
+    # Using greater than or equal to so that this doesn't need to be updated with every additional
+    # classifier to the seeds.
+    assert len(classifiers) >= 6
+    assert classifiers[0].id == 3
+    assert classifiers[1].id == 4
+    assert classifiers[2].id == 5
+    assert classifiers[3].id == 8
+    assert classifiers[4].id == 9
+    assert classifiers[5].id == 10
