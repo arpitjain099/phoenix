9e9da75950e3fdd91c909cf718f1ffdcc040dd3f
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-10-20 09:09:31 +0200

    PhiPhi: add deduplicate.refresh_deduplicated_authors_tables

--- python/projects/phiphi/phiphi/pipeline_jobs/constants.py
+++ python/projects/phiphi/phiphi/pipeline_jobs/constants.py
@@ -4,6 +4,7 @@
 GATHER_BATCHES_TABLE_NAME = "gather_batches"
 GENERALISED_MESSAGES_TABLE_NAME = "generalised_messages"
 DEDUPLICATED_GENERALISED_MESSAGES_TABLE_NAME = "deduped_generalised_messages"
+DEDUPLICATED_GENERALISED_AUTHORS_TABLE_NAME = "deduped_generalised_authors"
 CLASSIFIED_MESSAGES_TABLE_NAME = "classified_messages"
 TABULATED_MESSAGES_TABLE_NAME = "tabulated_messages"
 MANUALLY_CLASSIFIED_AUTHORS_TABLE_NAME = "manually_classified_authors"
