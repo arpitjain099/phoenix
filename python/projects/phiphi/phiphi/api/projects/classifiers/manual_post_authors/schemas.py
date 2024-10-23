81d65833a19c50340a2c77662da722e517550d90
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2024-10-23 11:43:37 +0200

    PhiPhi: add post intermediatory_classified_post_authors

--- python/projects/phiphi/phiphi/api/projects/classifiers/manual_post_authors/schemas.py
+++ python/projects/phiphi/phiphi/api/projects/classifiers/manual_post_authors/schemas.py
@@ -1,9 +1,28 @@
 """Manual posts authors schemas."""
+import datetime
 from typing import Literal, Optional
 
 import pydantic
 
 from phiphi.api.projects.classifiers import base_schemas
+
+
+class IntermediatoryClassifiedPostAuthorsCreate(pydantic.BaseModel):
+    """Intermediatory classified post authors create schema."""
+
+    class_id: int
+    phoenix_platform_message_author_id: str
+
+
+class IntermediatoryClassifiedPostAuthorsResponse(IntermediatoryClassifiedPostAuthorsCreate):
+    """Intermediatory classified post authors response schema."""
+
+    model_config = pydantic.ConfigDict(from_attributes=True)
+
+    id: int
+    classifier_id: int
+    created_at: datetime.datetime
+    # We don't return the updated at as classified_post_authors should not be updated.
 
 
 class ManualPostAuthorsVersionBase(pydantic.BaseModel):
